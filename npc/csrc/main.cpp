88636d42b75bc694f75d352c8bb58a8b9370ee6d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-30 15:39:46 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:39:46 up 15:28,  1 user,  load average: 1.16, 1.29, 1.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,41 @@
+#include "Vtop.h"
+#include <verilated.h>
+#include "verilated_vcd_c.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv) {
+    Verilated::mkdir("logs");
+
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    contextp->traceEverOn(true);
+    top->trace(tfp, 0);
+    tfp->open("logs/wave.vcd");
+
+
+    while (!contextp->gotFinish()) { 
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval(); 
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        // top->opentrace("trace.vcd");
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+
+        assert(top->f == (a ^ b));
+    }
+
+    tfp->close();
+
+    delete top;
+    delete contextp;
+    delete tfp;
+    return 0;
 }
