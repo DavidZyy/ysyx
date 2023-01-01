1512d8535f73253dd46b781eb4e2b4aef33b56c1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-01 22:53:23 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:53:23 up 1 day,  8:23,  2 users,  load average: 2.01, 2.02, 1.74

---
+++ npc/obj_dir/Vtop__Trace.cpp
@@ -0,0 +1,53 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vtop__Syms.h"
+
+
+void Vtop___024root__traceChgSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep);
+
+void Vtop___024root__traceChgTop0(void* voidSelf, VerilatedVcd* tracep) {
+    Vtop___024root* const __restrict vlSelf = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    if (VL_UNLIKELY(!vlSymsp->__Vm_activity)) return;
+    // Body
+    {
+        Vtop___024root__traceChgSub0((&vlSymsp->TOP), tracep);
+    }
+}
+
+void Vtop___024root__traceChgSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vluint32_t* const oldp = tracep->oldp(vlSymsp->__Vm_baseCode + 1);
+    if (false && oldp) {}  // Prevent unused
+    // Body
+    {
+        if (VL_UNLIKELY(vlSelf->__Vm_traceActivity[1U])) {
+            tracep->chgCData(oldp+0,(vlSelf->top__DOT____Vcellinp__i0____pinNumber3),4);
+            tracep->chgCData(oldp+1,(vlSelf->top__DOT__i0__DOT__i0__DOT__pair_list[0]),2);
+            tracep->chgCData(oldp+2,(vlSelf->top__DOT__i0__DOT__i0__DOT__pair_list[1]),2);
+            tracep->chgBit(oldp+3,(vlSelf->top__DOT__i0__DOT__i0__DOT__key_list[0]));
+            tracep->chgBit(oldp+4,(vlSelf->top__DOT__i0__DOT__i0__DOT__key_list[1]));
+            tracep->chgBit(oldp+5,(vlSelf->top__DOT__i0__DOT__i0__DOT__data_list[0]));
+            tracep->chgBit(oldp+6,(vlSelf->top__DOT__i0__DOT__i0__DOT__data_list[1]));
+            tracep->chgBit(oldp+7,(vlSelf->top__DOT__i0__DOT__i0__DOT__lut_out));
+            tracep->chgBit(oldp+8,(vlSelf->top__DOT__i0__DOT__i0__DOT__hit));
+        }
+        tracep->chgBit(oldp+9,(vlSelf->a));
+        tracep->chgBit(oldp+10,(vlSelf->b));
+        tracep->chgBit(oldp+11,(vlSelf->s));
+        tracep->chgBit(oldp+12,(vlSelf->y));
+    }
+}
+
+void Vtop___024root__traceCleanup(void* voidSelf, VerilatedVcd* /*unused*/) {
+    Vtop___024root* const __restrict vlSelf = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        vlSymsp->__Vm_activity = false;
+        vlSymsp->TOP.__Vm_traceActivity[0U] = 0U;
+        vlSymsp->TOP.__Vm_traceActivity[1U] = 0U;
+    }
+}
