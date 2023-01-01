1206191946197c2494d1689182bf3649e02c9586
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-01 22:09:24 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:24 up 1 day,  7:39,  2 users,  load average: 1.69, 2.00, 1.93

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,50 +1,4 @@
-/**
- * the main function used in simulation.
-*/
 
-// #include "Vtop.h"
-// #include <verilated.h>
-// #include "verilated_vcd_c.h"
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <assert.h>
-
-// void nvboard_bind_all_pins(Vtop* top);
-
-// int main(int argc, char** argv) {
-//     Verilated::mkdir("logs");
-
-//     VerilatedContext* contextp = new VerilatedContext;
-//     contextp->commandArgs(argc, argv);
-//     Vtop* top = new Vtop{contextp};
-
-//     VerilatedVcdC* tfp = new VerilatedVcdC;
-//     contextp->traceEverOn(true);
-//     top->trace(tfp, 0);
-//     tfp->open("logs/wave.vcd");
-
-
-//     while (!contextp->gotFinish()) { 
-//         int a = rand() & 1;
-//         int b = rand() & 1;
-//         top->a = a;
-//         top->b = b;
-//         top->eval(); 
-//         printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-//         // top->opentrace("trace.vcd");
-//         tfp->dump(contextp->time());
-//         contextp->timeInc(1);
-
-//         assert(top->f == (a ^ b));
-//     }
-
-//     tfp->close();
-
-//     delete top;
-//     delete contextp;
-//     delete tfp;
-//     return 0;
-// }
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include "../obj_dir/VMuxKey.h"
@@ -53,7 +7,7 @@
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
 
-static Vtop* top;
+static VMuxKey* top;
 
 void step_and_dump_wave(){
   top->eval();
@@ -63,7 +17,7 @@ void step_and_dump_wave(){
 void sim_init(){
   contextp = new VerilatedContext;
   tfp = new VerilatedVcdC;
-  top = new Vtop;
+  top = new VMuxKey;
   contextp->traceEverOn(true);
   top->trace(tfp, 0);
   tfp->open("dump.vcd");
