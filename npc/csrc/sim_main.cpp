7c966163d61ae3fcc44f334c16e86eaeca0351c3
DavidZyy <1929772352@qq.com>
2023-01-02 15:25:56 +0800

    finish 2 bit 4 choose 1 multiplexer

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,47 +1,42 @@
-/**
- * the main function used in simulation.
-*/
-
-#include "Vtop.h"
-#include <verilated.h>
+#include "verilated.h"
 #include "verilated_vcd_c.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include "../obj_dir/Vtop.h"
+#include <iostream>
 
-void nvboard_bind_all_pins(Vtop* top);
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
 
-int main(int argc, char** argv) {
-    Verilated::mkdir("logs");
+static Vtop* top;
 
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vtop* top = new Vtop{contextp};
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vtop;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+}
 
-    VerilatedVcdC* tfp = new VerilatedVcdC;
-    contextp->traceEverOn(true);
-    top->trace(tfp, 0);
-    tfp->open("logs/wave.vcd");
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
 
+int main() {
+  sim_init();
 
-    while (!contextp->gotFinish()) { 
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval(); 
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        // top->opentrace("trace.vcd");
-        tfp->dump(contextp->time());
-        contextp->timeInc(1);
-
-        assert(top->f == (a ^ b));
-    }
-
-    tfp->close();
-
-    delete top;
-    delete contextp;
-    delete tfp;
-    return 0;
+  top->s=0; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a和b均初始化为“0”
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，继续保持“0”，
+            top->a=1; top->b=0;  step_and_dump_wave();   // 将a，b分别改为“1”和“0”，s的值不变，
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，维持10个时间单位
+  top->s=1; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a，b分别变为“1,0,0”，维持10个时间单位
+                      top->b=1;  step_and_dump_wave();
+            top->a=1; top->b=0;  step_and_dump_wave();
+                      top->b=1;  step_and_dump_wave();
+  sim_exit();
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,8 +1,13 @@
-module top (
-    input a,
-    input b,
-    output f
-);
-    assign f = a ^ b;
+//键的数量为2，键的长度为1，数据的长度为1。
 
+module top(a,s,y);
+  input [7:0] a;
+  input [1:0] s;
+  output [1:0] y;
+  MuxKey #(4, 2, 2) i0 (y, s, {
+    2'b00, a[1:0],
+    2'b01, a[3:2],
+    2'b10, a[5:4],
+    2'b11, a[7:6]
+  });
 endmodule
