ac1794d4330a2a878e4928121517ef2c313cfef2
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-11 01:34:46 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:34:46 up 1 day,  4:48,  3 users,  load average: 0.81, 0.90, 0.83

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -131,6 +131,7 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("010000? ????? ????? 101 ????? 00100 11", srai   , I, R(dest) = (((int64_t) src1) >> RV64_shamt(imm)));
   INSTPAT("0000000 ????? ????? 001 ????? 00110 11", slliw  , I, R(dest) = SEXT(BITS(src1 << RV32_shamt(imm), 31, 0), 32)); /* use (int32_t) to replace BTIS(x, 31, 0) ? */
+  INSTPAT("0000000 ????? ????? 101 ????? 00110 11", srliw  , I, R(dest) = SEXT((uint32_t)src1 >> RV32_shamt(imm), 32)); /* use (int32_t) to replace BTIS(x, 31, 0) ? */
   INSTPAT("0100000 ????? ????? 101 ????? 00110 11", sraiw  , I, R(dest) = SEXT((int32_t)src1 >> RV32_shamt(imm), 32)); /* use (int32_t) to replace BTIS(x, 31, 0) ? */
 
   /* Integer Register-Register operations */
