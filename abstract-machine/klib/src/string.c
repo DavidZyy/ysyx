507ec5a6331a5a6951fc512f4139503676c5345a
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 20:31:39 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:31:39 up 1 day,  9:02,  1 user,  load average: 0.31, 0.48, 0.82

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,7 +52,7 @@ int strcmp(const char *s1, const char *s2) {
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   while(*s1 != '\0' && *s2 != '\0' && n){
-    if(!(*s1 - *s2)) return *s1 - *s2;
+    if((*s1 - *s2)) return *s1 - *s2;
     s1++;
     s2++;
     n--;
