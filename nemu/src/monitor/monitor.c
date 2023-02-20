f29dc4310f7d3065d32727e86761b27a661409ca
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-20 15:06:30 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:06:30 up  3:54,  1 user,  load average: 2.22, 2.74, 2.52

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -105,7 +105,7 @@ void init_monitor(int argc, char *argv[]) {
   parse_args(argc, argv);
 
   
-  printf(ANSI_FMT("%s\n", ANSI_FG_RED), img_file);
+  printf("image file:"ANSI_FMT("%s\n", ANSI_FG_RED), img_file);
 
   /* Set random seed. */
   init_rand();
