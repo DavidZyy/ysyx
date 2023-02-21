3a6046a1470a4a77d2a48cb381d2872cb222d761
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 20:15:57 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:15:57 up  3:03,  1 user,  load average: 0.74, 1.18, 1.38

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -37,7 +37,7 @@ decoder u_decoder(
   .inst_not_ipl ( inst_not_ipl)
 );
 
-always @(posedge clk) begin
+always @(*) begin
   if (inst_not_ipl) begin
     not_ipl_exception();
   end
@@ -87,7 +87,7 @@ Alu u_Alu(
 	.result     		( result     		)
 );
 
-always @(posedge clk) begin
+always @(*) begin
   if (is_ebreak) begin
     exit_code();
   end
