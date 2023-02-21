8d9c9138835b482f217be6eb39d63156147359c2
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 19:57:16 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:57:16 up  2:45,  1 user,  load average: 1.72, 1.97, 1.92

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -52,7 +52,7 @@ void exit_code(){
 void not_ipl_exception(){
   terminal = 1;
   printf(ANSI_FMT("instructions has not been immplemented!\n", ANSI_FG_RED));
-  printf(ANSI_FMT("pc: %p\n", ANSI_FG_RED), top->pc);
+  printf(ANSI_FMT("pc: %p\n", ANSI_FG_RED), (void *)top->pc);
 
 }
 
