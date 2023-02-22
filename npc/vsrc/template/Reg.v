92af06821b3479513cd7153cf607382d7fc8fe05
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 12:31:47 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:31:47 up 19:19,  1 user,  load average: 1.90, 2.04, 1.99

--- npc/vsrc/template/Reg.v
+++ npc/vsrc/template/Reg.v
@@ -7,7 +7,8 @@ module Reg #(parameter WIDTH = 1, parameter RESET_VAL = 0) (
 
   output reg [WIDTH-1:0] dout
 );
-  always @(posedge clk) begin
+  /* Trigger at negedge */
+  always @(negedge clk) begin
     if (rst) dout <= RESET_VAL;
     else if (wen) dout <= din;
   end
