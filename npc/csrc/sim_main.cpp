9e3fd69b1505f8b231087174ee0c23eeaa7c742e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 19:34:30 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:34:30 up 2 days, 23:36,  1 user,  load average: 1.60, 1.53, 1.80

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -109,6 +109,7 @@ void load_init_img(){
   *(uint32_t*)(&pmem[0]) = 0x00009117;
   *(uint32_t*)(&pmem[4]) = 0x00009117;
   *(uint32_t*)(&pmem[8]) = 0x00009117;
+  *(uint32_t*)(&pmem[8]) = 0x00008117;
   *(uint32_t*)(&pmem[12]) = ebreak;
 }
 
