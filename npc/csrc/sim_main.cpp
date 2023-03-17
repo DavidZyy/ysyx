c0d5333d6966c3d0208d5e559c03d992ab19051c
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 12:39:04 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:04 up 3 days, 16:41,  1 user,  load average: 3.28, 2.15, 1.84

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -133,7 +133,7 @@ void pmem_read(long long raddr, long long *rdata) {
   *rdata = *(uint64_t *)raddr_temp;
 }
 
-extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
+void pmem_write(long long waddr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
