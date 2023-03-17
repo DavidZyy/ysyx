4e8cdf342f9dfc1ebf30028bccbdc9dcca967dfc
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 12:19:41 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:19:41 up 3 days, 16:21,  1 user,  load average: 1.40, 1.58, 1.57

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -128,8 +128,9 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   raddr = raddr & ~0x7; // align to 8
-  (uint8_t *)raddr = guest_to_host(raddr);
-  *rdata = *(uint64_t *)raddr;
+
+  uint8_t *raddr_temp = guest_to_host(raddr);
+  *rdata = *(uint64_t *)raddr_temp;
 }
 
 extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
