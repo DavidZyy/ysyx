4950e86fb291bcbf04cbcb9900734815c31714aa
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 14:08:34 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:08:34 up 4 days, 18:10,  1 user,  load average: 1.18, 1.19, 1.38

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -134,7 +134,7 @@ end
 
 // two multiplexer
 assign next_pc = is_jal ? (current_pc + imm) : (current_pc + 4);
-assign next_pc = rst ? `PcRst : next_pc;
+// assign next_pc = rst ? `PcRst : next_pc;
 
 PC u_PC(
 	//ports
