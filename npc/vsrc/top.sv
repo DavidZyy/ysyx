0dff3391bed69ff6573a248ea5b4af418fcac990
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 15:32:38 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:38 up 4 days, 19:34,  1 user,  load average: 0.99, 1.56, 1.45

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -12,6 +12,7 @@ module top(
   input rst,
 
   output [`Vec(`ImmWidth)] current_pc,
+  /* verilator lint_off UNOPT */
   output [`Vec(`ImmWidth)] next_pc
 );
 
@@ -129,6 +130,7 @@ Alu u_Alu(
 // assign next_pc = is_jal ? (current_pc + imm) : (current_pc + 4);
 // assign next_pc = rst | is_jal ? `PcRst : next_pc;
 // assign next_pc = is_jal ? `PcRst  : 0;
+// assign next_pc = rst ? `PcRst : (is_jal ? (current_pc + imm) : (current_pc + 4));
 assign next_pc = rst ? `PcRst : (is_jal ? (current_pc + imm) : (current_pc + 4));
 
  Reg 
