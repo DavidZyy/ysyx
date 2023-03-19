94ddebc44a3f87067c4a852925fb8af322e38d7d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 14:32:52 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:32:52 up 5 days, 18:34,  1 user,  load average: 1.18, 1.25, 1.38

--- npc/csrc/include/macro.h
+++ npc/csrc/include/macro.h
@@ -4,6 +4,31 @@
 
 #define PG_ALIGN __attribute((aligned(4096)))
 
+#define grn(str) 	"\e[32;1m"str"\e[0m"
+#define ylw(str) 	"\e[33;1m"str"\e[0m"
+#define rd(str) 	"\e[31;1m"str"\e[0m"
+#define bl(str) 	"\e[34;1m"str"\e[0m"
+
+#define ANSI_FG_BLACK   "\33[1;30m"
+#define ANSI_FG_RED     "\33[1;31m"
+#define ANSI_FG_GREEN   "\33[1;32m"
+#define ANSI_FG_YELLOW  "\33[1;33m"
+#define ANSI_FG_BLUE    "\33[1;34m"
+#define ANSI_FG_MAGENTA "\33[1;35m"
+#define ANSI_FG_CYAN    "\33[1;36m"
+#define ANSI_FG_WHITE   "\33[1;37m"
+#define ANSI_BG_BLACK   "\33[1;40m"
+#define ANSI_BG_RED     "\33[1;41m"
+#define ANSI_BG_GREEN   "\33[1;42m"
+#define ANSI_BG_YELLOW  "\33[1;43m"
+#define ANSI_BG_BLUE    "\33[1;44m"
+#define ANSI_BG_MAGENTA "\33[1;35m"
+#define ANSI_BG_CYAN    "\33[1;46m"
+#define ANSI_BG_WHITE   "\33[1;47m"
+#define ANSI_NONE       "\33[0m"
+
+#define ANSI_FMT(str, fmt) fmt str ANSI_NONE
+
 #define inst_id (top->next_pc - 0x80000000)
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_MBASE 0x80000000
