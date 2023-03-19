064908d939f54a453ebd63fb0fb179fc5a88e5d9
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 13:44:16 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:16 up 5 days, 17:46,  1 user,  load average: 1.32, 1.22, 1.41

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -25,7 +25,8 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
-  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  void *ref_difftest_memcpy_temp = dlsym(handle, "difftest_memcpy");
+  ref_difftest_memcpy = (void (*)(paddr_t , void *, size_t , bool))ref_difftest_memcpy_temp;
   assert(ref_difftest_memcpy);
 
   ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
