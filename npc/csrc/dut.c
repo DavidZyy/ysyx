df5bbdab071fe111b76071643bc0552c02672273
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 11:39:27 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:39:27 up 5 days, 15:41,  1 user,  load average: 1.01, 1.18, 1.39

---
+++ npc/csrc/dut.c
@@ -0,0 +1,73 @@
+#include <dlfcn.h>
+
+#include <isa.h>
+#include <cpu/cpu.h>
+#include <memory/paddr.h>
+#include <utils.h>
+#include <difftest-def.h>
+
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+
+void init_difftest(char *ref_so_file, long img_size, int port) {
+  assert(ref_so_file != NULL);
+
+  void *handle;
+  handle = dlopen(ref_so_file, RTLD_LAZY);
+  assert(handle);
+
+  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  assert(ref_difftest_memcpy);
+
+  ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
+  assert(ref_difftest_regcpy);
+
+  ref_difftest_exec = dlsym(handle, "difftest_exec");
+  assert(ref_difftest_exec);
+
+  // ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+  // assert(ref_difftest_raise_intr);
+
+  void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
+  assert(ref_difftest_init);
+
+  // Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
+  // Log("The result of every instruction will be compared with %s. "
+  //     "This will help you a lot for debugging, but also significantly reduce the performance. "
+  //     "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
+
+  ref_difftest_init(port);
+  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+}
+
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  for(int i = 0; i < 32; i++){
+    if(ref_r->gpr[i] != cpu.gpr[i])
+      goto error;
+  }
+  if(ref_r->pc != cpu.pc)
+    goto error;
+
+  return true;
+error:
+  *(&pc) = ref_r->pc;
+  return false;
+}
+
+static void checkregs(CPU_state *ref, vaddr_t pc){
+  if (!isa_difftest_checkregs(ref, pc)) {
+    printf(ANSI_FMT("Regs error!", ANSI_FG_RED));
+    while(1);
+  }
+}
+
+void difftest_step(){
+  CPU_state ref_f;
+  int pc = 0;
+
+  ref_difftest_exec(1);
+  ref_difftest_regcpy(&ref_f, DIFFTEST_TO_DUT);
+  checkregs(&ref_f, pc);
+}
