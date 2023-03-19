300914ab441ecd7ca2a2ba2c4917c3e6c7290b16
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 12:27:39 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:27:39 up 5 days, 16:29,  1 user,  load average: 1.69, 1.10, 0.90

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -141,6 +141,19 @@ void pmem_write(long long waddr, long long wdata, char wmask) {
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
 }
 
+uint64_t *cpu_gpr = NULL;
+extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
+  cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
+}
+
+// 一个输出RTL中通用寄存器的值的示例
+void dump_gpr() {
+  int i;
+  for (i = 0; i < 32; i++) {
+    printf("gpr[%d] = 0x%lx\n", i, cpu_gpr[i]);
+  }
+}
+
 /**
  * The single cycle time series design refers:
  * https://nju-projectn.github.io/dlco-lecture-note/exp/11.html#id9
@@ -161,6 +174,7 @@ int main(int argc, char *argv[]) {
     // top->inst = *((uint32_t *)(&pmem[inst_id]));
     /* two cycle one instruction */
     single_cycle();
+    dump_gpr();
     // single_cycle();
     if(terminal)
       break;
