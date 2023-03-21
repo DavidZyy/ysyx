95a5fab257da173139fbf5a816bc59f9124a8ca1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 19:23:37 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:23:37 up 7 days, 23:25,  1 user,  load average: 1.46, 1.04, 1.06

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -148,7 +148,8 @@ Alu u_Alu(
 // assign next_pc_wire = rst ? `PcRst : (is_jal ? (current_pc + imm) : (current_pc + 4));
 // initial next_pc_wire = `PcRst;
 wire [`Vec(`ImmWidth)] next_pc_wire;
-assign next_pc_wire = (is_jal ? (current_pc + imm) : (current_pc + 4));
+// assign next_pc_wire = (is_jal ? (current_pc + imm) : (current_pc + 4));
+assign next_pc_wire = (is_jal ? (next_pc_reg + imm) : (next_pc_reg + 4));
 
 /* current instruction pc */
  Reg 
