cb10d941b6916a08d3db52941974809b12331f0f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 21:02:43 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:43 up 8 days,  1:04,  1 user,  load average: 1.96, 1.72, 1.42

--- npc/vsrc/template/Reg.v
+++ npc/vsrc/template/Reg.v
@@ -10,8 +10,8 @@ module Reg #(parameter WIDTH = 1, parameter RESET_VAL = 0) (
   /* Trigger at negedge */
   // always @(negedge clk) begin
   always @(negedge clk) begin
-    // if (rst) dout <= RESET_VAL;
-    // else if (wen) dout <= din;
-    if (wen) dout <= din;
+    if (rst) dout <= RESET_VAL;
+    else if (wen) dout <= din;
+    // if (wen) dout <= din;
   end
 endmodule
