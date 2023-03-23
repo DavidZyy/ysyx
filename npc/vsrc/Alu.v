dd680f0d378e3e81c5f616b28d8388db85aa7bf2
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-23 22:22:13 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:13 up 10 days,  2:24,  1 user,  load average: 1.49, 1.45, 1.19

--- npc/vsrc/Alu.v
+++ npc/vsrc/Alu.v
@@ -39,7 +39,7 @@ module Alu (
         `AluNe,     `ZEXT(operator_1 != operator_2, 1),
         `AluGe,     `ZEXT($signed(operator_1) >= $signed(operator_2), 1),
         `AluGeu,    `ZEXT($unsigned(operator_1) >= $unsigned(operator_2), 1),
-        `Aluaddw,   `SEXT((operator_1 + operator_2)[31:0], 32)
+        `AluAddw,   `SEXT((operator_1 + operator_2)[31:0], 32)
         })
         );
 endmodule //Alu
