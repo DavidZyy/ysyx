6aac51639f7d5cdb5965d4069d30d0eba9072695
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 16:29:54 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:29:54 up  4:38,  1 user,  load average: 1.57, 1.45, 1.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,24 +7,85 @@
 
 #define max_char_cnt  256
 /* */
+// int printf(const char *fmt, ...) {
+//   char buffer[max_char_cnt];
+//   memset(buffer, 0, max_char_cnt);
+// 
+//   va_list args;
+//   va_start(args, fmt);
+//   /* not right pass args */
+//   int char_cnt = sprintf(buffer, fmt, args);
+//   if(char_cnt > max_char_cnt) {
+//     panic("printf out of bound!");
+//   }
+//   va_end(args);
+// 
+//   for(int i = 0; buffer[i] != '\0'; i++)
+//     putch(buffer[i]);
+// 
+//   return 0;
+// }
+
 int printf(const char *fmt, ...) {
-  char buffer[max_char_cnt];
-  memset(buffer, 0, max_char_cnt);
+  char buffer[256];
+  char *out = buffer;
+  // char *origin_out = out;
+  va_list ap;
+  va_start(ap, fmt);
+  int d, p;
+  char *str;
 
-  va_list args;
-  va_start(args, fmt);
-  /* not right pass args */
-  int char_cnt = sprintf(buffer, fmt, args);
-  if(char_cnt > max_char_cnt) {
-    panic("printf out of bound!");
+  while(*fmt != '\0'){
+    if(*fmt == '%'){
+      fmt++;
+      switch (*fmt) {
+       case 's':
+         str = va_arg(ap, char*);
+         strcpy(out, str);
+         out += strlen(str);
+         break;
+       
+       case 'd':
+         d = va_arg(ap, int);
+         /* change int to char* */
+         char temp_buf[32];
+         p = 0;
+         while (d)
+         {
+           int rem = d%10;
+           d = d/10;
+           char ch = (char)((int)'0' + rem);
+           temp_buf[p++] = ch;
+         }
+         p--;
+         while(p >= 0){
+           *out = temp_buf[p];
+           out++;
+           p--;
+         }
+         break;
+
+       default:
+         panic("Not implemented");
+         break;
+      }
+    }
+    else{
+      *out = *fmt;
+      out++;
+    }
+    fmt++;
   }
-  va_end(args);
+  *out = '\0';
 
+  va_end(ap);
+  /* the chars copyed */
+  // return out - origin_out;
   for(int i = 0; buffer[i] != '\0'; i++)
     putch(buffer[i]);
-
   return 0;
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
