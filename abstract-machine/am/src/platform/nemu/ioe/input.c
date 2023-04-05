0da5617f380696a5573f687a55ddca45dbcaa52c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 20:32:23 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:23 up  1:02,  1 user,  load average: 0.97, 0.86, 1.05

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,7 +3,14 @@
 
 #define KEYDOWN_MASK 0x8000
 
+static uint32_t read_key(){
+  uint32_t key = *(volatile uint32_t *)KBD_ADDR;
+  return key;
+}
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key = read_key();
+  kbd->keydown = key & KEYDOWN_MASK;
+  // kbd->keycode = AM_KEY_NONE;
+  kbd->keycode = (uint16_t)key;
 }
--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,3 +1,4 @@
+/*use other printf lib */
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
