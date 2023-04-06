446ec5dd4bccd62c705b42b8c629cd78f813b33d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 21:34:12 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:12 up  2:48,  1 user,  load average: 1.17, 1.10, 0.89

--- npc/csrc/include/utils.h
+++ npc/csrc/include/utils.h
@@ -19,8 +19,8 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 void difftest_step();
 
 /* mem.cpp */
-uint8_t* guest_to_host(paddr_t paddr);
-paddr_t host_to_guest(uint8_t *haddr);
-void pmem_read(long long raddr, long long *rdata);
-void pmem_write(long long waddr, long long wdata, char wmask);
+// uint8_t* guest_to_host(paddr_t paddr);
+// paddr_t host_to_guest(uint8_t *haddr);
+// void pmem_read(long long raddr, long long *rdata);
+// void pmem_write(long long waddr, long long wdata, char wmask);
 #endif
