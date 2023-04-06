1cc9609dca4c24afc0086ef0e897fb52a2aa344a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 20:44:45 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:45 up  1:58,  1 user,  load average: 0.55, 0.58, 0.53

---
+++ npc/vsrc/rom.sv
@@ -0,0 +1,25 @@
+// also use DPI-C machenism, may be use ip-core when vivado?
+
+`include "./include/defines.v"
+
+import "DPI-C" function void pmem_read(
+  input longint mem_raddr, output longint rinst);
+
+module rom (
+    input      pc,
+
+    output [`Vec(`InstWidth)] inst,
+);
+    
+    localparam mask = 64'h7;
+
+    wire [`Vec(`RegWidth)] rinst;
+    assign inst = (pc & mask) == 0 ? rinst[`Vec(`InstWidth)] : rinst[63:32];
+
+    /* We should read instructions immediately when pc changes. */
+    always @(*) begin
+      pmem_read(pc, rinst);
+    end
+
+
+endmodule //rom
