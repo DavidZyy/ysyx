c931258a607ea97aba26f1ca815506d9bd2f0639
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 20:58:36 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:36 up  2:12,  1 user,  load average: 0.42, 0.60, 0.67

---
+++ npc/csrc/include/mem.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <utils.h>
+
+uint8_t* guest_to_host(paddr_t paddr);
+paddr_t host_to_guest(uint8_t *haddr);
+void pmem_read(long long raddr, long long *rdata);
+void pmem_write(long long waddr, long long wdata, char wmask);
---
+++ npc/csrc/mem.cpp
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <utils.h>
+
+uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+
+/* my understanding: paddr is the program in the guest's address, paddr - CONFIG_MBASE 
+is the offset from the beginning of the program, pmem is the beginning of the program in
+the host. */
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+/* haddr is the address in host nemu, - pmem is the offset from the beginning, + CONFIG_MBASE(the
+beginning of the programm in guest) get the address in the guest. */
+paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+
+
+static inline bool in_pmem(paddr_t addr) {
+  return (addr >= CONFIG_MBASE) && (addr - CONFIG_MBASE < CONFIG_MSIZE);
+}
+
+void pmem_read(long long raddr, long long *rdata) {
+  // printf(ANSI_FMT("current_pc: %p\n", ANSI_FG_RED), (void *)top->current_pc);
+  // printf(ANSI_FMT("next_pc: %p\n", ANSI_FG_RED), (void *)top->next_pc);
+  // printf(ANSI_FMT("raddr: %p\n\n", ANSI_FG_RED), raddr);
+  // if(top->rst){
+  if(raddr <= 0x40){
+    *rdata = 0;
+    return;
+  }
+  // if(top->next_pc && top->current_pc)
+    assert(in_pmem(raddr));
+  // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
+  raddr = raddr & ~0x7; // align to 8
+
+  uint8_t *raddr_temp = guest_to_host(raddr);
+  *rdata = *(uint64_t *)raddr_temp;
+}
+
+void pmem_write(long long waddr, long long wdata, char wmask) {
+  assert(in_pmem(waddr));
+
+  // assert(!(waddr & 0x7));
+  // printf("waddr: %lx\n", waddr);
+  // printf("wdata: %lx\n", wdata);
+  // printf("wmask: %lx\n", wmask);
+  // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
+  // `wmask`中每比特表示`wdata`中1个字节的掩码,
+  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+  uint8_t *waddr_temp = guest_to_host(waddr);
+  switch (wmask) {
+    case 0x1:   *(uint8_t  *)waddr_temp = wdata;break;
+    case 0x3:   *(uint16_t *)waddr_temp = wdata;break;
+    case 0xf:   *(uint32_t *)waddr_temp = wdata;break;
+    // case 0xff:  *(uint64_t *)waddr_temp = wdata;break;
+    default:  *(uint64_t *)waddr_temp = wdata;break;
+    // default: printf("pmem_write!\n"); exit(0); break;
+    // default: printf("pmem_write!\n"); break;
+  }
+}
