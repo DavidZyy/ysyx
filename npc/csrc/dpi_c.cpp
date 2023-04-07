c3533936c1d89a94044718fa730b8479718b80f6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 13:59:42 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:42 up  1:19,  1 user,  load average: 1.05, 0.31, 0.24

---
+++ npc/csrc/dpi_c.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file dpi_c.cpp
+ * @author Yangyang Zhu (1929772352@qq.com)
+ * @version 0.1
+ * @date 2023-04-07
+ * 
+ * @copyright Copyright (c) 2023
+ * other dpi-c functions except memory related functions.
+ */
