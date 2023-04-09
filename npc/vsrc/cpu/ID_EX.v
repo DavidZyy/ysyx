701c9a81018a94282dfa77ff47ae573386042446
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 16:17:58 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:58 up  6:40,  1 user,  load average: 0.64, 0.95, 1.06

--- npc/vsrc/cpu/ID_EX.v
+++ npc/vsrc/cpu/ID_EX.v
@@ -1,14 +1,26 @@
-// 
-// `include "./include/defines.v"
-// 
-// module ID_EX (
-//     input      clk,
-//     input      rst,
-//     input      [`Vec(`AluopWidth)] alu_op_ID,
-//     input      [`Vec(`WdtTypeCnt)] wdt_op_ID,
-//     input      [`Vec(`SigOpWidth)] sig_op_ID,
-//     input      [`Vec(`ImmWidth)]	 imm_ID,
-//     
-// );
-//     
-// endmodule //ID_EX
+
+`include "./include/defines.v"
+
+module ID_EX (
+    input      clk,
+    input      rst,
+    input      [`Vec(`AluopWidth)] alu_op_ID,
+    input      [`Vec(`WdtTypeCnt)] wdt_op_ID,
+    input      [`Vec(`SigOpWidth)] sig_op_ID,
+    input      [`Vec(`ImmWidth)]	 imm_ID,
+    input      [`Vec(`ImmWidth)]	 rdata_1_ID,
+    input      [`Vec(`ImmWidth)]	 rdata_2_ID,
+    input      [`Vec(`ImmWidth)]   pc_ID,
+    input      [`Vec(`InstWidth)]  inst_ID,
+    
+    output      [`Vec(`AluopWidth)]  alu_op_EX,
+    output      [`Vec(`WdtTypeCnt)]  wdt_op_EX,
+    output      [`Vec(`SigOpWidth)]  sig_op_EX,
+    output      [`Vec(`ImmWidth)]	   imm_EX,
+    output      [`Vec(`ImmWidth)]	   rdata_1_EX,
+    output      [`Vec(`ImmWidth)]	   rdata_2_EX,
+    output      [`Vec(`ImmWidth)]    pc_EX,
+    output      [`Vec(`InstWidth)]   inst_EX
+);
+    
+endmodule //ID_EX
