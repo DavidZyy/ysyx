f4c157f680c0685f098fc27143e435b8398155f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 11:47:31 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:31 up  2:09,  1 user,  load average: 1.29, 1.09, 1.02

--- npc/vsrc/cpu/ID_EX.v
+++ npc/vsrc/cpu/ID_EX.v
@@ -4,9 +4,9 @@
 module ID_EX (
     input      clk,
     input      rst,
-    input  [`Vec(`AluopWidth)] alu_op;
-    input  [`Vec(`WdtTypeCnt)] wdt_op;
-    input  [`Vec(`SigOpWidth)] sig_op_ID;
+    input  [`Vec(`AluopWidth)] alu_op,
+    input  [`Vec(`WdtTypeCnt)] wdt_op,
+    input  [`Vec(`SigOpWidth)] sig_op_ID,
     input  [`Vec(`ImmWidth)]	 imm,
     
 );
