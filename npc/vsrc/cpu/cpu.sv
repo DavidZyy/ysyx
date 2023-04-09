d4d03d003a1c0a32744296c6e3a3bf04367ea9e3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 11:32:03 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:03 up  1:54,  1 user,  load average: 0.89, 0.85, 0.86

--- npc/vsrc/cpu/cpu.sv
+++ npc/vsrc/cpu/cpu.sv
@@ -54,6 +54,58 @@ IF_ID u_IF_ID (
   .IF_ID_inst (IF_ID_inst)
 );
 
+/* decode instructionn stage */
+wire [`Vec(`RegIdWidth)]	rd;
+wire [`Vec(`RegIdWidth)]	rs1;
+wire [`Vec(`RegIdWidth)]	rs2;
+wire [`Vec(`ImmWidth)]	imm;
+
+/* signals */
+wire  [`Vec(`AluopWidth)] alu_op;
+wire [`Vec(`WdtTypeCnt)] wdt_op;
+wire [`Vec(`SigOpWidth)] sig_op_ID;
+
+decoder u_decoder(
+	//ports
+	.inst     		    ( IF_ID_inst ),
+
+	.rd       		    ( rd       		),
+	.rs1      		    ( rs1      		),
+	.rs2      		    ( rs2      		),
+	.imm      		    ( imm      		),
+  .alu_op           ( alu_op      ),
+  .wdt_op           ( wdt_op),
+  .sig_op_ID        ( sig_op_ID )
+
+);
+
+/* execute stage */
+wire [`Vec(`ImmWidth)]	reg_wdata = (sig_op_ID[`SIG_OP_is_jal] | sig_op_ID[`SIG_OP_is_jalr]) ? 
+                                    (IF_ID_pc + 4) : 
+                                    (sig_op_ID[`SIG_OP_is_load] ? extended_data : alu_result);
+
+wire [`Vec(`ImmWidth)]	rdata_1;
+wire [`Vec(`ImmWidth)]	rdata_2;
+
+  /* in execute state, read register, in WB state, write back registers */
+RegisterFile 
+#(
+  .ADDR_WIDTH (`RegIdWidth),
+  .DATA_WIDTH (`RegWidth)
+)
+u_RegisterFile(
+  .clk        ( clk     ),
+  .reg_wdata  ( reg_wdata   ),
+  .rd         ( rd      ),
+  .reg_wen    ( sig_op_ID[`SIG_OP_reg_wen]     ),
+  .rs1        ( rs1 ),
+  .rs2        ( rs2 ),
+
+  .rdata_1    ( rdata_1 ),
+  .rdata_2    ( rdata_2 )
+);
+
+
 /* ram */
 memory u_memory (
 	//ports
@@ -80,30 +132,7 @@ load_extend u_load_extend (
 	.extended_data 		( extended_data 		)
 );
 
-/* decode instructionn stage */
-wire [`Vec(`RegIdWidth)]	rd;
-wire [`Vec(`RegIdWidth)]	rs1;
-wire [`Vec(`RegIdWidth)]	rs2;
-wire [`Vec(`ImmWidth)]	imm;
 
-/* signals */
-wire  [`Vec(`AluopWidth)] alu_op;
-wire [`Vec(`WdtTypeCnt)] wdt_op;
-wire [`Vec(`SigOpWidth)] sig_op_ID;
-
-decoder u_decoder(
-	//ports
-	.inst     		    ( IF_ID_inst ),
-
-	.rd       		    ( rd       		),
-	.rs1      		    ( rs1      		),
-	.rs2      		    ( rs2      		),
-	.imm      		    ( imm      		),
-  .alu_op           ( alu_op      ),
-  .wdt_op           ( wdt_op),
-  .sig_op_ID        ( sig_op_ID )
-
-);
 
 /*suppose one cycle is begin with the negtive cycle. 
   can not use negedge, because when in the edge of 
@@ -137,31 +166,7 @@ end
     // $display("pc: %x inst: %x", current_pc, inst);
 // end
 
-/* execute stage */
-wire [`Vec(`ImmWidth)]	reg_wdata = (sig_op_ID[`SIG_OP_is_jal] | sig_op_ID[`SIG_OP_is_jalr]) ? 
-                                    (IF_ID_pc + 4) : 
-                                    (sig_op_ID[`SIG_OP_is_load] ? extended_data : alu_result);
 
-wire [`Vec(`ImmWidth)]	rdata_1;
-wire [`Vec(`ImmWidth)]	rdata_2;
-
-  /* in execute state, read register, in WB state, write back registers */
-RegisterFile 
-#(
-  .ADDR_WIDTH (`RegIdWidth),
-  .DATA_WIDTH (`RegWidth)
-)
-u_RegisterFile(
-  .clk        ( clk     ),
-  .reg_wdata  ( reg_wdata   ),
-  .rd         ( rd      ),
-  .reg_wen    ( sig_op_ID[`SIG_OP_reg_wen]     ),
-  .rs1        ( rs1 ),
-  .rs2        ( rs2 ),
-
-  .rdata_1    ( rdata_1 ),
-  .rdata_2    ( rdata_2 )
-);
 
   /* input */
 wire [`Vec(`ImmWidth)]  operator_1 = (sig_op_ID[`SIG_OP_is_auipc] | sig_op_ID[`SIG_OP_is_jal]) ? 
