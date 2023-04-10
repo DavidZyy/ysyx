53f095e9c50cf0997966b8af322815748161cb1e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 15:31:43 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:31:43 up  3:59,  1 user,  load average: 0.95, 0.72, 0.54

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -146,7 +146,7 @@ int main(int argc, char *argv[]) {
   uint64_t times = -1;
 
   // int nemu_not_run = 1;
-  int begin = 1;
+  int begin = 0;
 
   for(i = 0; i < times; i++){
     npc_exec_once();
