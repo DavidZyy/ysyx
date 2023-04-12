44e5cab6a25ec275dbf50bd018c2123107b15533
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 19:46:35 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:46:35 up 2 days,  8:14,  1 user,  load average: 0.29, 0.54, 0.75

--- npc/vsrc/cpu/EX_MEM.v
+++ npc/vsrc/cpu/EX_MEM.v
@@ -2,7 +2,13 @@
 `include "./include/defines.v"
 
 module EX_MEM (
-  
+  input [`Vec(`SigOpWidth)]	sig_op_EX,
+  input [`Vec(`WdtTypeCnt)]	wdt_op_EX,
+  input [`Vec(`ImmWidth)]	alu_result_EX,
+
+  output [`Vec(`SigOpWidth)]	sig_op_MEM,
+  output [`Vec(`WdtTypeCnt)]	wdt_op_MEM,
+  output [`Vec(`ImmWidth)]	alu_result_MEM
 );
     
 endmodule //EX_MEM
