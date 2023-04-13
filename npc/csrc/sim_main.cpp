fecb3f3a6fff648a4a60e09f87d191c89f905143
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 10:47:25 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:25 up 2 days, 23:15,  1 user,  load average: 0.65, 0.83, 0.90

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -155,6 +155,7 @@ int main(int argc, char *argv[]) {
       npc_exec_once(); // execute jmp / branch
       npc_exec_once(); // execute nop
       npc_exec_once(); // execute nop
+      npc_exec_once(); // execute nop
     }
     else {
       npc_exec_once();
