0d1ea5ed7af59416577235918429b2b77980f4a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 13:34:05 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:05 up 3 days,  2:01,  1 user,  load average: 1.95, 1.66, 1.23

--- npc/vsrc/cpu/cpu.sv
+++ npc/vsrc/cpu/cpu.sv
@@ -133,13 +133,15 @@ wire [`Vec(`RegIdWidth)]	rd_EX;
 wire flush_EX_temp;
 
 /* branch not write rd */
-wire [`Vec(`ImmWidth)]	rdata_1_ID = ((~rdata_1_forward_EX) | ~sig_op_EX[`SIG_OP_reg_wen]) ? 
-                                      rdata_1 : 
-                                      (sig_op_EX[`SIG_OP_is_load] ? mem_rdata_extended : alu_result_EX);
+// wire [`Vec(`ImmWidth)]	rdata_1_ID = ((~rdata_1_forward_EX) | ~sig_op_EX[`SIG_OP_reg_wen]) ? 
+                                      // rdata_1 : 
+                                      // (sig_op_EX[`SIG_OP_is_load] ? mem_rdata_extended : alu_result_EX);
 
-// wire [`Vec(`ImmWidth)]	rdata_1_ID = (rdata_1_forward_EX && sig_op_EX[`SIG_OP_reg_wen])?
-                                      // alu_result_EX :
-                                      // ()
+wire [`Vec(`ImmWidth)]	rdata_1_ID = (rdata_1_forward_EX && sig_op_EX[`SIG_OP_reg_wen]) ?
+                                      alu_result_EX :
+                                      ((rdata_1_forward_MEM && sig_op_MEM[`SIG_OP_reg_wen]) ?
+                                        ((sig_op_MEM[`SIG_OP_is_load]) ? mem_rdata_extended : alu_result_MEM) : 
+                                        rdata_1);
 
 wire [`Vec(`ImmWidth)]	rdata_2_ID = ((~rdata_2_forward_EX) | ~sig_op_EX[`SIG_OP_reg_wen]) ? 
                                       rdata_2 : 
