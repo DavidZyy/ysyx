2333e07365a0e0834057842e56de2958b6ad1f8f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 17:28:29 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:29 up 3 days,  5:56,  1 user,  load average: 0.89, 1.02, 1.42

--- npc/vsrc/cpu/MEM_WB.v
+++ npc/vsrc/cpu/MEM_WB.v
@@ -2,12 +2,19 @@
 `include "./include/defines.v"
 
 module MEM_WB (
-    input      clk,
-    input      rst,
-i[`Vec(`ImmWidth)] mem_rdata_ex_MEM;
-    input [`Vec(`ImmWidth)]   pc_MEM,
-    input [`Vec(`InstWidth)]  inst_MEM,
+    input clk,
+    input rst,
+    input flush_MEM,      
+    input [`Vec(`ImmWidth)]    mem_rdata_ex_MEM,
+    input [`Vec(`ImmWidth)]	   alu_result_MEM,
+    input [`Vec(`SigOpWidth)]  sig_op_MEM,
+    input [`Vec(`ImmWidth)]    pc_MEM,
+    input [`Vec(`InstWidth)]   inst_MEM,
 
+    output flush_WB,
+    output [`Vec(`ImmWidth)]   mem_rdata_ex_WB,
+    output [`Vec(`ImmWidth)]	 alu_result_WB,
+    output [`Vec(`SigOpWidth)] sig_op_WB,
     output [`Vec(`ImmWidth)]   pc_WB,
     output [`Vec(`InstWidth)]  inst_WB
 );
