1e431996f24e32bcf0628f4c0c233612bf50bc31
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 17:05:36 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:36 up 3 days,  5:33,  1 user,  load average: 3.34, 2.07, 1.65

--- npc/vsrc/cpu/MEM_WB.v
+++ npc/vsrc/cpu/MEM_WB.v
@@ -4,7 +4,7 @@
 module MEM_WB (
     input      clk,
     input      rst,
-wire [`Vec(`ImmWidth)] mem_rdata_ex_MEM;
+i[`Vec(`ImmWidth)] mem_rdata_ex_MEM;
     input [`Vec(`ImmWidth)]   pc_MEM,
     input [`Vec(`InstWidth)]  inst_MEM,
 
