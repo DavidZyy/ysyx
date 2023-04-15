cc425556eeda863bc413af0ab94f83aebd8f9778
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-15 16:47:12 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:12 up 1 day,  4:21,  1 user,  load average: 1.99, 1.96, 1.91

--- npc/vsrc/cpu/rom.sv
+++ npc/vsrc/cpu/rom.sv
@@ -14,10 +14,20 @@ module rom (
     wire [`Vec(`RegWidth)] rinst;
     assign inst = (pc & mask) == 0 ? rinst[`Vec(`InstWidth)] : rinst[63:32];
 
+
     /* We should read instructions immediately when pc changes. */
-    always @(*) begin
-      pmem_read(pc, rinst);
+    // always @(*) begin
+      // pmem_read(pc, rinst);
+    // end
+
+
+    reg [63:0] rom_mem[8192-1:0]; 
+    
+    initial begin
+      $readmemh("/home/zhuyangyang/project/ysyx-workbench/npc/vsrc/cpu/hello.hex", rom_mem);
     end
+    
 
-
+    wire [`Vec(`RegWidth)] shift_pc = pc >> 3;
+    assign rinst = rom_mem[shift_pc[12:0]][63:0];
 endmodule //rom
