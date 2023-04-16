7cf837e9bfee642acd560c3adcae6cd7a33760c9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 18:06:29 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:29 up  7:23,  1 user,  load average: 1.36, 1.14, 0.95

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -12,16 +12,23 @@ AM_SRCS := riscv/npc/start.S \
            platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
-LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-# LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+# LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
+# image: $(IMAGE).elf
+# 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+# 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+# 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
-	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+	@$(OBJCOPY) $(IMAGE).elf --dump-section .mytext="$(IMAGE).rom.bin" --dump-section .rodata="$(IMAGE).ram.bin"
+	@od -w4 -An --endian little -v -t x4 "$(IMAGE).rom.bin" > "$(IMAGE).rom.hex"
+	@od -w4 -An --endian little -v -t x4 "$(IMAGE).ram.bin" > "$(IMAGE).ram.hex"
 
 # $(error $(IMAGE))
 # relative path
