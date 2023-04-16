70eaf0b9c021d9610788fe665817384eb2039a39
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 13:19:01 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:19:01 up  2:35,  1 user,  load average: 1.24, 1.65, 1.66

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -19,7 +19,7 @@ CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
 image: $(IMAGE).elf
-	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@$(OBJDUMP) -s $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
