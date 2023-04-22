22bef2da728bb53ae31d916982048527d732e109
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 13:56:52 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:56:52 up 1 day,  1:22,  1 user,  load average: 3.71, 2.10, 1.34

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -29,7 +29,9 @@ image: $(IMAGE).elf
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 	@$(OBJCOPY) $(IMAGE).elf --dump-section .mytext="$(IMAGE).rom.bin" \
-	--dump-section .rodata="$(IMAGE).ram.bin"
+	--dump-section .rodata="$(IMAGE).ram.bin" \
+	--dump-section .srodata.mainargs="$(IMAGE).ram.bin" \
+	--dump-section .srodata.cst8="$(IMAGE).ram.bin"
 	# @$(OBJCOPY) $(IMAGE).elf --dump-section .mytext="$(IMAGE).rom.bin" --dump-section .riscv.attributes="$(IMAGE).ram.bin"
 	@od -w4 -An --endian little -v -t x4 "$(IMAGE).rom.bin" > "$(IMAGE).rom.hex"
 	@od -w4 -An --endian little -v -t x4 "$(IMAGE).ram.bin" > "$(IMAGE).ram.hex"
