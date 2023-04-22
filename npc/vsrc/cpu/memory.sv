723837ec5d5e8f37604cb17e5f5ee4df4a5a5a5c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 19:54:53 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:53 up 1 day,  7:20,  1 user,  load average: 0.57, 0.45, 0.51

--- npc/vsrc/cpu/memory.sv
+++ npc/vsrc/cpu/memory.sv
@@ -250,8 +250,8 @@ module memory (
     /* check if write correct */
     always @(negedge clk or posedge clk) begin
         if(mem_wen) begin
-          width_64_out_1[31:0]  = ram_mem[ram_waddr[addr_width-1:0]][31:0];
-          width_64_out_1[63:32] = ram_mem[ram_waddr[addr_width-1:0] + 1][31:0];
+          width_64_out_1[31:0]  <= ram_mem[ram_waddr[addr_width-1:0]][31:0];
+          width_64_out_1[63:32] <= ram_mem[ram_waddr[addr_width-1:0] + 1][31:0];
           pmem_read(mem_waddr, width_64_out_2);
           if(width_64_out_1 != width_64_out_2)
             // exit_code()
