edb63a3411e0044a5f2077c0badda8ea053d5254
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 16:42:21 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:21 up 1 day,  4:07,  1 user,  load average: 1.59, 1.14, 1.06

--- npc/vsrc/cpu/memory.sv
+++ npc/vsrc/cpu/memory.sv
@@ -200,7 +200,8 @@ module memory (
     wire [`Vec(`RegWidth)] sub_waddr   = mem_waddr - `RamAddr;
     wire [`Vec(`RegWidth)] shift_waddr = sub_waddr >> 2;
 
-    always @(negedge clk) begin
+    // always @(negedge clk) begin
+    always @(posedge clk) begin
       if(mem_wen)
         pmem_write(mem_waddr, mem_wdata, wmask);
       else
