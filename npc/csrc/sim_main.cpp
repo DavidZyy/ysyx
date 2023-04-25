125ae449e56388f5c2e920c051fd684a1fff3e22
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 20:17:52 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:52 up  1:27,  1 user,  load average: 0.83, 0.65, 0.65

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -171,7 +171,7 @@ void kdb_sendcode() {
 
 // 当nemu的pc和npc的pc_IF为xxxx时，说明这个地址的指令还没有执行。
 /** * The single cycle time series design refers: * https://nju-projectn.github.io/dlco-lecture-note/exp/11.html#id9 */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
   // get_cpu();
   cpu.pc = RESET_VECTOR;
   // print_arg(argc, argv);
@@ -193,7 +193,7 @@ int main(int argc, char *argv[]) {
   // int nemu_not_run = 1;
   int begin = 1;
 
-  for(i = 0;  < times; i++){
+  for(i = 0; i < times; i++){
 
     /* if control branch failed */
     if(begin){
