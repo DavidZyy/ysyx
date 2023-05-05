201478efdded13e71c4405c9542d6f0b508fb1b1
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 20:35:21 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:21 up  1:28,  1 user,  load average: 2.82, 2.69, 2.47

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -70,9 +70,18 @@ static inline void update_screen() {
 #endif
 #endif
 
+#define SYNC_ADDR (VGACTL_ADDR + 4)
+/**
+ * where is the sync register?
+ */
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  if(1){
+    update_screen();
+  }
+  
+  
 }
 
 void init_vga() {
