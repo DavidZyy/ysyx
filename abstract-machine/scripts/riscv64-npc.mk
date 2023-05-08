dc343a4765ad1cda7de60a20cf10d3a6f7f85e1f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-08 16:36:17 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:17 up 18:43,  1 user,  load average: 0.58, 0.63, 1.02

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -12,8 +12,9 @@ AM_SRCS := riscv/npc/start.S \
            platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
-# LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+$(info sss)
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+# LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
