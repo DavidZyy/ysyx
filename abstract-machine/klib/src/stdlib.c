db5117b8bc3d0c507b625bc3d548c9d6f7c024b3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 20:47:59 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:59 up 10:14,  1 user,  load average: 0.85, 0.75, 0.96

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,6 +29,8 @@ int atoi(const char* nptr) {
   return x;
 }
 
+// void *addr = heap.start;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
