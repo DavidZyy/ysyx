85da7dcc730e57d9caca3bab490ad34d2da05bdc
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 21:24:37 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:37 up 10:51,  1 user,  load average: 2.47, 2.05, 1.55

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -38,8 +38,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   // fb += ctl->x * ctl->y;
   int a = ctl->x * ctl->y;
   int b = ctl->w * ctl->h;
-  // for (i = 0; i < ctl->w * ctl->h; i ++) fb[a + i] = ctl_pixels[i];
-  for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[b-1-i];
+  for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[i];
+  // for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[b-1-i];
   if (ctl->sync) {
     // nemu no this func
     outl(SYNC_ADDR, 1);
