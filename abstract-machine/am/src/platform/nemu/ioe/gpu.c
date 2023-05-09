de6e9eb79e4a1a0fa2dd6a1b745ae055b4c2a334
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 21:49:51 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:51 up 11:16,  1 user,  load average: 1.33, 1.17, 1.40

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -34,35 +34,35 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // int i;
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // uint32_t *ctl_pixels = (uint32_t *)ctl->pixels;
-  // // fb += ctl->x * ctl->y;
-  // int a = ctl->x * ctl->y;
-  // int b = ctl->w * ctl->h;
-  // for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[i];
-  // // for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[b-1-i];
-  // if (ctl->sync) {
-  //   // nemu no this func
-  //   outl(SYNC_ADDR, 1);
-  // }
-  int x = ctl->x;
-  int y = ctl->y;
-    uint32_t *pixels = ctl->pixels;
-    int w = ctl->w;
-    int h = ctl->h;
-
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    for (int i = 0; i < h; i++)
-    {
-        memcpy(&fb[(y + i) * 400 + x], pixels, 4 * w);
-        pixels += w;
-    }
-
-    if (ctl->sync)
-    {
-        outl(SYNC_ADDR, 1);
-    }
+  int i;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *ctl_pixels = (uint32_t *)ctl->pixels;
+  // fb += ctl->x * ctl->y;
+  int a = ctl->x * ctl->y;
+  int b = ctl->w * ctl->h;
+  for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[i];
+  // for (i = 0; i < b; i ++) fb[a + i] = ctl_pixels[b-1-i];
+  if (ctl->sync) {
+    // nemu no this func
+    outl(SYNC_ADDR, 1);
+  }
+//   int x = ctl->x;
+//   int y = ctl->y;
+//     uint32_t *pixels = ctl->pixels;
+//     int w = ctl->w;
+//     int h = ctl->h;
+// 
+//     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//     for (int i = 0; i < h; i++)
+//     {
+//         memcpy(&fb[(y + i) * 400 + x], pixels, 4 * w);
+//         pixels += w;
+//     }
+// 
+//     if (ctl->sync)
+//     {
+//         outl(SYNC_ADDR, 1);
+//     }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
