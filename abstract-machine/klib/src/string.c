e4608adc8cc192255a5bb34a8c3c96c0e115bb1e
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 16:28:13 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:13 up  2:11,  1 user,  load average: 0.65, 0.88, 1.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -103,12 +103,12 @@ void *memmove(void *dst, const void *src, size_t n) {
 //   }
 //   return out;
 // }
+
 void *memcpy(void *out, const void *in, size_t n)
 {
     char *dst = out;
     const char *src = in;
-    while (n > 0)
-    {
+    while (n > 0) {
         *dst++ = *src++;
         n--;
     }
