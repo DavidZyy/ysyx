57eec467905c4556b14ba2949283fb0dddc711fe
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 17:37:14 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:37:14 up 1 day,  3:20,  1 user,  load average: 0.95, 0.76, 1.11

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -184,6 +184,9 @@ int main(int argc, char *argv[]) {
   top->rst = 1;
   single_cycle(0);
   single_cycle(0);
+  single_cycle(0);
+  single_cycle(0);
+  single_cycle(0);
   // single_cycle(0);
   top->rst = 0;
 
