602aaf0d75a644256c750d29a421ca799253bc43
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 20:02:44 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:02:44 up  6:16,  1 user,  load average: 1.48, 0.95, 0.81

--- npc/vsrc/cpu/ram.sv
+++ npc/vsrc/cpu/ram.sv
@@ -71,7 +71,7 @@ module ram (
 
     /* time se*/
     always @(posedge clk) begin
-    always @(*) begin
+    // always @(*) begin
         if (`InMem(mem_raddr, `ADDR_RAM, `RAM_LEN))
         if(mem_ren) begin
           width_64_out[31:0]  <= ram_mem[ram_raddr[addr_width-1:0]][31:0];
