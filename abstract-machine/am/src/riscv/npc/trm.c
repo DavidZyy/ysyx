85e6bd3c7fb222fd2a114d4e0f588443431394ec
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 16:08:23 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:23 up  2:22,  1 user,  load average: 0.83, 1.21, 1.15

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <npc.h>
 
 extern char _heap_start;
 int main(const char *args);
@@ -15,6 +16,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
+  outb((0xa0000000 + 0x00003f8), ch);
 }
 
 // // use inline assembly codes
