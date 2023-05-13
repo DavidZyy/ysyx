ca30100795820b9d49717630bd266e728561bd89
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 19:40:00 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:00 up 1 day,  5:54,  1 user,  load average: 1.43, 1.12, 0.83

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -63,11 +63,11 @@ module top	(
     wire [`Vec(8)]	swt_rdata;
     cpu u_cpu (
             //ports
-            // .clk        		( clkdiv[0]		    ), // 200 0000 / (2^27)
+            .clk        		( clkdiv[0]		    ), // 200 0000 / (2^27)
             // .clk        		( clk200m			), /* for simulation on varilator */
             .clkdiv             ( clkdiv            ),
             /* 10 can run on sword */
-            .clk        		( clkdiv[10]		),
+            // .clk        		( clkdiv[10]		),
             // .clk        		( btn_clk			),
             /* use switch as reset? */
             .rst        		( rst        		),
