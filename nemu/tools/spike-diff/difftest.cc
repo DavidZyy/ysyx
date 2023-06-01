00f62aaba9c2c9bbc63511d662ba0c225d376c26
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 19:28:43 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:43 up 1 day,  1:56,  1 user,  load average: 0.44, 0.66, 0.72

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -51,7 +51,7 @@ void sim_t::diff_init(int port) {
   p = get_core("0");
   state = p->get_state();
   // pa3 
-  state->mstatus = 0xa00001800;
+  // state->mstatus = 0xa00001800;
 }
 
 void sim_t::diff_step(uint64_t n) {
