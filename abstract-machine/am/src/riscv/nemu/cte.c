837b7b69b982a9b045e0764d982b6981ce644c9b
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-02 21:51:30 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:30 up 2 days,  4:18,  1 user,  load average: 0.44, 0.54, 0.56

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -30,8 +30,8 @@ void print_context(Context *c) {
 // new added
 
 Context* __am_irq_handle(Context *c) {
-  // add mepc 4
-  // c->mepc += 4;
+  // add mepc 4, do by software
+  c->mepc += 4;
   print_context(c);
   if (user_handler) {
     Event ev = {0};
