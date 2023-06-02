3a8de0d4a3ff7935b6967668f503b911623de4b5
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-02 11:29:56 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:56 up 1 day, 17:57,  1 user,  load average: 0.42, 0.46, 0.56

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,6 +4,21 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+// new added
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void print_context(Context *c) {
+  for(int i = 0; i < 32; i++) {
+    printf("%-3s: %016lx  ", regs[i], c->gpr[i]);
+  }
+}
+// new added
+
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
