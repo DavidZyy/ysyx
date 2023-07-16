72a3011fa6806ba36f259a7926a6f93c40aad855
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-16 18:43:56 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:56 up 2 days,  7:37,  1 user,  load average: 3.34, 1.94, 1.78

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -232,7 +232,7 @@ bool check_parentheses(int p, int q) {
 word_t eval(int p, int q) {
   if(p > q) {
     printf("p: %d, q: %d\n", p, q);
-    assert(0);
+    panic("ilegal expression!\n");
     // return 0;
   } else if (p == q) {
     // operands
