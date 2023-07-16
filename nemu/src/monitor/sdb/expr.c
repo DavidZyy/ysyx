88f53457e583f1058dca667cdb56060e158ec214
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-16 17:24:51 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:24:51 up 2 days,  6:18,  1 user,  load average: 2.39, 2.12, 2.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -253,7 +253,12 @@ word_t eval(int p, int q) {
     // operators
     int op = getop(p, q);
     int val1 = eval(p, op-1);
-    int val2 = eval(op+1, q);
+    int val2;
+    if(tokens[op+1].type == TK_MINUS) {
+      val2 = -eval(op+2, q);
+    } else {
+      val2 = eval(op+1, q);
+    }
 
     switch (tokens[op].type) {
       case '+': return val1 + val2;
