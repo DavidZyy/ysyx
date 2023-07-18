52cb0b1fdce03f14b6698a6f44725ae22f019985
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-18 15:58:46 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:46 up 4 days,  4:52,  1 user,  load average: 0.33, 0.52, 0.59

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,21 +31,71 @@ static char *code_format =
 "  return 0; "
 "}";
 
+int buf_id;
+
+int choose(int n) {
+  return rand() % n;
+}
+
+void put_str_in_buf(char *str) {
+  sprintf(buf+buf_id, "%s", str);
+  buf_id += strlen(str);
+}
+
+void put_char_in_buf(char ch) {
+  sprintf(buf+buf_id, "%c", ch);
+  buf_id++;
+}
+
+void gen(char ch) {
+  put_char_in_buf(ch);
+}
+
+void gen_num() {
+  int num = choose(100);
+  char str[10];
+  sprintf(str, "%d", num);
+  put_str_in_buf(str);
+}
+
+int num_operators = 4;
+void gen_rand_op() {
+  int operator_index = rand() % num_operators;
+  char operator;
+  switch (operator_index) {
+    case 0: operator = '+'; break;
+    case 1: operator = '-'; break;
+    case 2: operator = '*'; break;
+    case 3: operator = '/'; break;
+    default: operator = '+';
+  }
+  put_char_in_buf(operator);
+}
+
 static void gen_rand_expr() {
   // buf[0] = '\0';
-  sprintf(buf, "%s", "(1 + 2) * (3-1)");
+  // sprintf(buf, "%s", "1");
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
   int a = 0;
   int seed = time(0);
   srand(seed);
-  int loop = 1;
+  int loop = 10;
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    // reset
+    memset(buf, 0, sizeof(buf));
+    buf_id  = 0;
+
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
