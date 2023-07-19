2297d5df0ca992c5854b6b4b3086a5d1ea6b0cb7
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-19 12:59:55 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:59:55 up 20:06,  3 users,  load average: 1.07, 1.07, 0.97

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -226,7 +226,7 @@ bool check_parentheses(int p, int q) {
 
   if(stack_id == 0)
     parentheses_correct = 1;
-  // printf("\nstack_id: %d\n", stack_id);
+  printf("\nstack_id: %d\n", stack_id);
 
   if(parentheses_correct) {
     if(match_p_q) {
