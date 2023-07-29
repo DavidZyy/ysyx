1483bf05f629bc9e6b49530359b80812ed5a80d1
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-29 11:12:56 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:12:56 up 14:22,  1 user,  load average: 1.10, 1.11, 0.97

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -24,5 +24,5 @@ int is_exit_status_bad() {
   // int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     // (nemu_state.state == NEMU_QUIT);
   // return !good;
-  return 0;
+  return 1;
 }
