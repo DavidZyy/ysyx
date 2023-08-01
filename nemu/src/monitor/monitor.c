1ae8b56b0f2454bd5dc2ad48b2188cf33cc6ebb1
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-01 21:45:52 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:45:52 up 1 day,  3:15,  4 users,  load average: 3.72, 3.14, 2.70

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -107,11 +107,13 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
-  // log_write("%s\n", elf_file);
+  log_write("%s\n", elf_file);
 
   
   // printf("image file:"ANSI_FMT("%s\n", ANSI_FG_RED), img_file);
-  printf("elf file:"ANSI_FMT("%s\n", ANSI_FG_RED), elf_file);
+  // printf("elf file:"ANSI_FMT("%s\n", ANSI_FG_RED), elf_file);
+
+  // init_elf();
 
   /* Set random seed. */
   init_rand();
