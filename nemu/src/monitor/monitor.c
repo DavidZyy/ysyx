6779cd95d7c74517a6f4bea20bdb5c6d103fb949
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-02 22:55:09 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:55:09 up 2 days,  4:24,  4 users,  load average: 2.82, 2.10, 2.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -158,8 +158,10 @@ void init_elf(const char* elf_file) {
   int num_symbols = symtab.sh_size / sizeof(Elf64_Sym);
   assert(num_symbols < sizeof(symbols) / sizeof(Elf64_Sym));
   for (int i = 1; i < num_symbols; i++) {
+    if(symbols[i].st_name)
       Log("Symbol %d: Name=%s, Value=0x%lx, Size=%lu\n", i,
              section_names + symbols[i].st_name, symbols[i].st_value, symbols[i].st_size);
+            //  NULL, symbols[i].st_value, symbols[i].st_size);
   }
 
   // free(section_names);
