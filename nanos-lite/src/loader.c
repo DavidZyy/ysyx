f5cad07ee3fb9f19f7847e16c2f79f71d6608d90
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-04 15:05:10 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:05:10 up 1 day, 15:06,  4 users,  load average: 1.12, 1.21, 1.34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -57,7 +57,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
 
   // return 0;
-  return 0x83000000;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
