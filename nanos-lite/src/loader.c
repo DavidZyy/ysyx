5666338a740d7f39429cf9e06fdba875570de16c
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-04 11:58:26 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:58:26 up 1 day, 11:59,  4 users,  load average: 2.35, 2.32, 2.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,12 +9,34 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+void printf_elf_header(Elf64_Ehdr elf_header){
+  printf("ELF Header Information:\n");
+  printf("  Magic: ");
+  for (int i = 0; i < 16; i++) {
+      printf("%02x ", elf_header.e_ident[i]);
+  }
+  printf("\n");
+  printf("  Type: %u\n", elf_header.e_type);
+  printf("  Machine: %u\n", elf_header.e_machine);
+  printf("  Version: %u\n", elf_header.e_version);
+  printf("  Entry point address: 0x%lx\n", elf_header.e_entry);
+  printf("  Program header offset: %lu\n", elf_header.e_phoff);
+  printf("  Section header offset: %lu\n", elf_header.e_shoff);
+  printf("  Flags: %u\n", elf_header.e_flags);
+  printf("  ELF header size: %u\n", elf_header.e_ehsize);
+  printf("  Program header entry size: %u\n", elf_header.e_phentsize);
+  printf("  Program header entry count: %u\n", elf_header.e_phnum);
+  printf("  Section header entry size: %u\n", elf_header.e_shentsize);
+  printf("  Section header entry count: %u\n", elf_header.e_shnum);
+  printf("  Section header string table index: %u\n", elf_header.e_shstrndx);
+}
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
-  char buf[sizeof(Elf_Ehdr)];
-  ramdisk_read(buf, 0, sizeof(Elf_Ehdr));
-  printf("hello\n");
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  printf_elf_header(ehdr);
   return 0;
 }
 
