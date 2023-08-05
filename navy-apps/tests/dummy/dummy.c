e2a63438afcbc85305a8e594a307730493c20a94
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-05 15:59:16 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:59:16 up  6:54,  3 users,  load average: 1.60, 1.80, 1.79

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -9,5 +9,5 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
   // return _syscall_(SYS_yield, 0, 0, 0);
-  return _syscall_(1, 1, 0, 0);
+  return _syscall_(10, 1, 0, 0);
 }
