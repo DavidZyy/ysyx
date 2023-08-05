5e7ddfd0d89eb392365a8f904fdcabbfdc19eb1a
DavidZyy <1929772352@qq.com>
2023-08-05 20:19:59 +0800

    switch ysyx 6

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,11 +9,55 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+void print_elf_header(Elf_Ehdr elf_header) {
+  printf("ELF Header Information:\n");
+  printf("  Magic: ");
+  for (int i = 0; i < 16; i++) {
+      printf("%02x ", elf_header.e_ident[i]);
+  }
+  printf("\n");
+  printf("  Type: %u\n", elf_header.e_type);
+  printf("  Machine: %u\n", elf_header.e_machine);
+  printf("  Version: %u\n", elf_header.e_version);
+  printf("  Entry point address: 0x%lx\n", elf_header.e_entry);
+  printf("  Program header offset: %lu\n", elf_header.e_phoff);
+  printf("  Section header offset: %lu\n", elf_header.e_shoff);
+  printf("  Flags: %u\n", elf_header.e_flags);
+  printf("  ELF header size: %u\n", elf_header.e_ehsize);
+  printf("  Program header entry size: %u\n", elf_header.e_phentsize);
+  printf("  Program header entry count: %u\n", elf_header.e_phnum);
+  printf("  Section header entry size: %u\n", elf_header.e_shentsize);
+  printf("  Section header entry count: %u\n", elf_header.e_shnum);
+  printf("  Section header string table index: %u\n", elf_header.e_shstrndx);
+}
+
+void print_program_header(Elf_Phdr program_header) {
+  printf("Type: %u ", program_header.p_type);
+  printf("Offset: 0x%lx ", program_header.p_offset);
+  printf("Virtual Address: 0x%lx ", program_header.p_vaddr);
+  printf("Physical Address: 0x%lx ", program_header.p_paddr);
+  printf("File Size: 0x%lx ", program_header.p_filesz);
+  printf("Memory Size: 0x%lx ", program_header.p_memsz);
+  printf("Flags: 0x%x ", program_header.p_flags);
+  printf("Align: 0x%lx\n", program_header.p_align);
+}
+
+extern uint8_t ramdisk_start;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
-  // char buf[sizeof(Elf_Ehdr)];
-  // ramdisk_read(buf, 0, sizeof(Elf_Ehdr));
-  return 0;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint64_t *)ehdr.e_ident == 0x00010102464c457f);
+  // print_elf_header(ehdr);
+
+  for(int i=0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    ramdisk_read(&phdr, ehdr.e_phoff + i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    // print_program_header(phdr);
+  }
+
+  return (uintptr_t )(&ramdisk_start + ehdr.e_entry - 0x83000000);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,8 +3,13 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    // case 1: yield(); break;
+    // default: panic("Unhandled syscall ID = %d %d %d %d", a[0], a[1], a[2], a[3]);
+    // default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
