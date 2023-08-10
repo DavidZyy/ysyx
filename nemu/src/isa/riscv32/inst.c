c20bb028e6f6b8b8cd7d01418c7cb1e143182d76
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-10 13:12:13 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:12:13 up 13:26,  4 users,  load average: 0.37, 0.66, 0.54

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -58,17 +58,17 @@ void ecall(Decode *s);
 void mret(Decode *s);
 
 word_t mulhu(word_t src1, word_t src2) {
-  // volatile uint64_t a = (uint64_t)src1;
-  // volatile uint64_t b = (uint64_t)src2;
-  // volatile uint64_t c = a * b;
+  volatile uint64_t a = (uint64_t)src1;
+  volatile uint64_t b = (uint64_t)src2;
+  volatile uint64_t c = a * b;
+  c = c >> 32;
+  return (word_t)c;
+
+  // volatile word_t a = src1>>16;
+  // volatile word_t b = src2>>16;
+  // volatile word_t c = a * b;
   // c = c >> 32;
   // return (word_t)c;
-
-  volatile word_t a = src1>>16;
-  volatile word_t b = src2>>16;
-  volatile word_t c = a * b;
-  // c = c >> 32;
-  return (word_t)c;
 }
 
 static int decode_exec(Decode *s) {
