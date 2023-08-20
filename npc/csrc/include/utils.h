21794b525e5e4709da5d7af9691b3aef8cde59d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-20 22:50:57 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:50:57 up 2 days,  8:27,  4 users,  load average: 2.20, 1.37, 1.17

--- npc/csrc/include/utils.h
+++ npc/csrc/include/utils.h
@@ -2,17 +2,6 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include <macro.h>
-
-typedef struct {
-  uint64_t gpr[32];
-  // uint64_t *gpr;
-  /* paddinig */
-  // uint64_t padding[31];
-  vaddr_t pc;
-} riscv64_CPU_state;
-
-typedef riscv64_CPU_state CPU_state;
 
 
 void init_difftest(char *ref_so_file, long img_size, int port);
