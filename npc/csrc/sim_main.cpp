6e5601930e79215c3e7d7b4c49799b4e190e8752
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-21 00:09:59 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:09:59 up 2 days,  9:46,  1 user,  load average: 0.84, 1.08, 0.85

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -100,7 +100,8 @@ void print_arg(int argc, char *argv[]){
 
 uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
-  cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
+  // cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
+  cpu_gpr = (word_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
 // 一个输出RTL中通用寄存器的值的示例
