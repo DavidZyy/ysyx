accb66d8268dd4fbac17a449e79be1f95f3dc17d
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-21 13:13:14 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:13:14 up 2 days, 22:49,  1 user,  load average: 1.56, 1.62, 1.44

--- npc/csrc/include/mem.h
+++ npc/csrc/include/mem.h
@@ -6,7 +6,7 @@
 // paddr_t host_to_guest(uint8_t *haddr);
 // void pmem_read(long long raddr, long long *rdata);
 // void pmem_read(word_t raddr, word_t *rdata);
-void pmem_read(int raddr, int *rdata);
+extern "C" void pmem_read(int raddr, int *rdata);
 // void pmem_write(long long waddr, long long wdata, char wmask);
 
 extern uint8_t pmem[CONFIG_MSIZE];
