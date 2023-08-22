f7189c3594cd0c0642181ab3bc6502711677e9a6
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-22 14:18:30 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:18:30 up 3 days, 23:55,  1 user,  load average: 1.50, 1.57, 1.29

--- npc/vsrc/RomBB.v
+++ npc/vsrc/RomBB.v
@@ -4,12 +4,13 @@ import "DPI-C" function void pmem_read(
 //   input int mem_waddr, input int wdata, input byte wmask);
 
 module RomBB (
+    input clock,
     input [31:0] addr,
     output [31:0] inst
 );
 
     /* We should read instructions immediately when pc changes. */
-    always @(addr ) begin
+    always @(posedge clock) begin
       pmem_read(addr, inst);
     end
 
