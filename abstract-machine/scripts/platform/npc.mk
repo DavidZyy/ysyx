432c2c020bff7f4c00b59e0b2417f334d4a223f8
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-23 23:45:34 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:45:34 up 5 days,  9:22,  1 user,  load average: 1.44, 1.37, 1.26

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -9,10 +9,10 @@ AM_SRCS := riscv/npc/start.S \
            platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
-# LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-						#  --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 
-LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld \
+# LDFLAGS   += -T $(AM_HOME)/scripts/linker2.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 
 LDFLAGS   += --gc-sections -e _start
