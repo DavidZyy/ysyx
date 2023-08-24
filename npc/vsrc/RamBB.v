bd3afc94527b4994061991036debfb6a86f981ca
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-24 16:30:59 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:59 up 6 days,  2:07,  1 user,  load average: 1.63, 1.76, 1.57

--- npc/vsrc/RamBB.v
+++ npc/vsrc/RamBB.v
@@ -18,8 +18,8 @@ module RamBB (
     output reg [`DATA_WIDTH-1:0] rdata
 );
 
-always @(*) begin
-// always @(negedge clock) begin
+// always @(*) begin
+always @(negedge clock) begin
   if (valid) begin // 有读写请求时
     vaddr_read(addr, rdata);
     if (mem_wen) begin // 有写请求时
