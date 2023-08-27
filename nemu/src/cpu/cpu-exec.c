e9a00ab60f4ce9071a292d37cbf0d0084b3ecb97
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-28 02:17:34 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:17:34 up  2:31,  3 users,  load average: 0.39, 0.39, 0.53

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -144,7 +144,7 @@ void cpu_exec(uint64_t n) {
   g_timer += timer_end - timer_start;
 
 
-  // print_iringbuf();
+  print_iringbuf();
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
