9432ecf90d19ed88264d3e8a326f59771141fae6
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-28 01:26:00 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:26:00 up  1:39,  3 users,  load average: 1.13, 1.54, 1.62

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ void do_syscall(Context *c) {
 
   Log("syscall ID = %d %d %d %d", a[0], a[1], a[2], a[3]);
   switch (a[0]) {
-    case SYS_exit:
+    // case SYS_exit:
 
     case SYS_yield: 
       // yield(); 
