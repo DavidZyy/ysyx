b1285d43ab61566e3df88bc78f5086153efb774f
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-28 15:37:40 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:40 up 15:51,  5 users,  load average: 0.66, 0.90, 0.98

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -91,9 +91,9 @@ void sim_t::diff_set_regs(void* diff_context) {
   state->pc = ctx->pc;
   state->mtvec   = ctx->csr[cpu_mtvec_id]   ;
   state->mepc    = ctx->csr[cpu_mepc_id]    ;
-  state->mstatus = ctx->csr[cpu_mstatus_id] ;
+  // state->mstatus = ctx->csr[cpu_mstatus_id] ;
   state->mcause  = ctx->csr[cpu_mcause_id]  ;
-  // state->mstatus = ctx->mstatus ;
+  state->mstatus = ctx->mstatus ;
   // state->mstatus = 0;
 }
 
