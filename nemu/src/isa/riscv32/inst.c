9ce674118ed9b8c8bb93eeddb0c912b55b641a5c
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-28 15:01:22 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:22 up 15:15,  5 users,  load average: 1.22, 0.73, 0.63

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -201,7 +201,7 @@ void ecall(Decode *s) {
   cpu.csr[cpu_mepc_id]    = cpu.pc; // see ref
   cpu.csr[cpu_mcause_id]  = 0xb; // environment call from M-mode
   s->dnpc = cpu.csr[cpu_mtvec_id];
-  cpu.mstatus.fields.mpp = 3;
+  cpu.mstatus.fields.mpp = 1;
 }
 
 void csrrs(word_t csr_id, int rd, word_t src1) {
