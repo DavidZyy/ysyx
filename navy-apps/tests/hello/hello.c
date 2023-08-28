f2630bc51bc440bd352f78e037730c99de126b25
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-28 23:30:03 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:30:03 up 23:43,  4 users,  load average: 1.60, 1.11, 0.80

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,16 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
+extern _end;
+
 int main() {
   write(1, "Hello World!\n", 13);
-  int i = 2;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-  }
+  write(1, "_end is %d", _end);
+  // int i = 2;
+  // volatile int j = 0;
+  // while (1) {
+  //   j ++;
+  //   if (j == 10000) {
+  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  //     j = 0;
+  //   }
+  // }
   return 0;
 }
