567f0eff948ae30fb277e29445fd11fcd7e1a9eb
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 13:23:25 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:25 up 1 day, 13:37,  4 users,  load average: 1.16, 0.94, 0.82

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,3 +34,9 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
