69e6ff95580cbba603f3a26aa0bbc46a550deeb4
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 21:14:01 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:01 up 1 day, 21:27,  4 users,  load average: 0.64, 0.54, 0.73

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,10 +12,10 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   // assert(offset + len <= RAMDISK_SIZE);
-  if(offset + len > RAMDISK_SIZE) {
-    printf("offset: %d, len: %d\n", offset, len);
-    panic("ramdisk_read faild");
-  }
+  // if(offset + len > RAMDISK_SIZE) {
+  //   printf("offset: %d, len: %d\n", offset, len);
+  //   panic("ramdisk_read faild");
+  // }
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
