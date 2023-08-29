1195014c4015f256f333acdd59a1619de5f73fa8
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 17:11:55 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:55 up 1 day, 17:25,  4 users,  load average: 1.67, 1.02, 0.84

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -53,7 +53,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   size_t offset = file_table[fd].open_offset;
   file_table[fd].open_offset += len;
-  // assert(file_table[fd].open_offset <= file_table[fd].disk_offset + file_table[fd].size);
+  assert(file_table[fd].open_offset <= file_table[fd].disk_offset + file_table[fd].size);
   return ramdisk_read((void *)buf, offset, len);
 }
 
