62b0254b68f96406035c971288c1e1ae135d4fd2
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-30 21:37:11 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:11 up 2 days, 21:51,  4 users,  load average: 0.95, 1.62, 2.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,8 +35,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int h = io_read(AM_GPU_CONFIG).height;
 
   assert(len >= 30);
-  snprintf(buf, len, "WIDTH: %d\nHEIGHT: %d\n", w, h);
-  return 0;
+  return snprintf(buf, len, "WIDTH: %d\nHEIGHT: %d\n", w, h);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
