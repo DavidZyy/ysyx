cd8c789b1274934d5bdd0c494d3bd5035a3bf2d9
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-30 22:25:17 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:17 up 2 days, 22:39,  4 users,  load average: 0.75, 1.02, 1.12

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -68,7 +68,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0);
   /* write line by line */
   for(int i = 0; i < h; i++) {
-    lseek(fd, sizeof(int) * ((y+i)*canvas_w + x), SEEK_SET);
+    lseek(fd, sizeof(int) * ((y+i)*screen_w + x), SEEK_SET);
     write(fd, pixels, w*sizeof(int));
     pixels += w;
   }
