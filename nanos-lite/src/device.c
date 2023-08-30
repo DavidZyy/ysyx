1add22a0723b1eac6da0b593fb00b43860b0e75d
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-30 23:04:33 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:04:33 up 2 days, 23:18,  4 users,  load average: 1.33, 1.31, 1.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,7 +49,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
   // assert(x + pixels_len <= w);
   io_write(AM_GPU_FBDRAW, x, y, (uint32_t *)buf, pixels_len, 1, true);
-  Log("w %d off %d, len %d",w, pixels_offset, pixels_len);
+  Log("x %d y %d w %d off %d, len %d", x, y, w, pixels_offset, pixels_len);
   return len;
 }
 
