52b559b58259d1b4d7109be4c1dd2df5d37a46f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-03 14:09:54 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:09:54 up 1 day,  2:04,  3 users,  load average: 1.34, 2.04, 1.46

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,7 +17,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  char buf[4];
+  char buf[64];
   if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
   }
