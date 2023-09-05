a70d2f50bbc0afbb28b4ef53988bbec07d4f8459
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-05 22:22:15 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:15 up 10:32,  4 users,  load average: 1.79, 2.05, 1.66

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[]) {
 static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
   SDL_Surface *s = BDF_CreateSurface(font, ch, fg, bg);
   SDL_Rect dstrect = { .x = x, .y = y };
+  printf("s: %x\n", s);
   assert(s);
   assert(screen);
   SDL_BlitSurface(s, NULL, screen, &dstrect);
