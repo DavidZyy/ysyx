66e4ac524bd770ddc8778a1c3ccdf56207f40d3d
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-05 23:02:09 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:02:09 up 11:12,  4 users,  load average: 1.59, 1.47, 1.38

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -29,10 +29,10 @@ int main(int argc, char *argv[]) {
 }
 
 static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
-  printf("ch is %d\n", ch);
+  // printf("ch is %d\n", ch);
   // assert(BDF_CreateSurface(font, ch, fg, bg));
   SDL_Surface *s = BDF_CreateSurface(font, ch, fg, bg);
-  printf("in draw ch s: %x\n", s);
+  // printf("in draw ch s: %x\n", s);
   SDL_Rect dstrect = { .x = x, .y = y };
   assert(s);
   assert(screen);
