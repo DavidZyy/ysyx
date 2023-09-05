795b7f1b7fda8c3ffd846f012d840c7dc8be1ae7
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-05 23:03:08 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:03:08 up 11:13,  4 users,  load average: 1.48, 1.47, 1.38

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -45,7 +45,7 @@ void refresh_terminal() {
   for (int i = 0; i < W; i ++)
     for (int j = 0; j < H; j ++)
       if (term->is_dirty(i, j)) {
-        if(!term->getch(i, j)) continue;
+        // if(!term->getch(i, j)) continue;
         draw_ch(i * font->w, j * font->h, term->getch(i, j), term->foreground(i, j), term->background(i, j));
         needsync = 1;
       }
