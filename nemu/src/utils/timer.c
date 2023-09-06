dcf9b52c9651e99a57c17b9517b7bd7c3bfc1344
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 17:36:46 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:46 up  4:38,  6 users,  load average: 0.78, 0.55, 0.52

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -34,6 +34,7 @@ static uint64_t get_time_internal() {
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+    Log("get_time_internal: %ld", us);
 #endif
   return us;
 }
