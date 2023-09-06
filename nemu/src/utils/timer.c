5d03e0c995b231371facde178f27f4df83badc9c
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 17:39:48 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:48 up  4:41,  6 users,  load average: 0.32, 0.43, 0.47

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -42,5 +42,6 @@ static uint64_t get_time_internal() {
 uint64_t get_time() {
   if (boot_time == 0) boot_time = get_time_internal();
   uint64_t now = get_time_internal();
+  Log("get_time: %ld", now - boot_time);
   return now - boot_time;
 }
