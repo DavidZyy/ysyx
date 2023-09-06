3526358e1c9a61ca87481daf79209c61a781b75b
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 16:18:33 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:33 up  3:20,  6 users,  load average: 0.23, 0.41, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -32,9 +32,11 @@ void __am_timer_init() {
   boot_time = read_time();
 }
 
+#include<stdio.h>
 /* return the us */
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = read_time() - boot_time;
+  printf("%ld", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
