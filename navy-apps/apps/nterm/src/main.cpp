38763165397d987d7ebfdfc1d5a92997b115cafb
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-09 17:45:14 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:45:14 up 19:31,  4 users,  load average: 1.64, 1.37, 1.13

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
   // setup display
   int win_w = font->w * W;
   int win_h = font->h * H;
-  screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
+  // screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
 
   term = new Terminal(W, H);
 
