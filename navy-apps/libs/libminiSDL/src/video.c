e947dc00262238b3eb2400cfd75e27a455be19ee
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-09 21:28:50 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:28:50 up  2:14,  4 users,  load average: 0.21, 0.32, 0.35

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -51,7 +51,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
       // *(dst_px + i*screen_w + j) = *(src_px);
       
-      check_vmem(dst, dst_px + i*dst->w + j);
+      check_vmem(dst, dst_px + i*screen_w + j);
       *(dst_px + i*screen_w + j) = *(src_px);
       src_px++;
 
