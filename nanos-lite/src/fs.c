f54dc93ed1cef966a13943027f5a4bb25d21c073
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-10 16:02:33 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:02:33 up  5:44,  5 users,  load average: 0.38, 0.66, 0.51

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -140,8 +140,7 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
   // Check if the open offset is within bounds
   // assert(file_table[fd].open_offset <= file_table[fd].disk_offset + file_table[fd].size);
 
-  // return file_table[fd].open_offset;
-  return file_table[fd].size;
+  return file_table[fd].open_offset - file_table[fd].disk_offset;
 }
 
 int fs_close(int fd) {
