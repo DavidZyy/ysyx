d94b2e36ed1be7a091eefdc472f072147db950f1
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-16 10:37:27 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:37:27 up 11:14,  4 users,  load average: 1.87, 1.56, 1.51

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -21,24 +21,17 @@ int SDL_PollEvent(SDL_Event *event) {
   memset(event, 0, sizeof(event));
   if (NDL_PollEvent(buf, sizeof(buf))) {
     // printf("%s", buf);
-    if(buf[0] == 'k' && buf[1] == 'd') {
-      event->type = SDL_KEYDOWN;
-      for(int i = 0; i < sizeof(keyname)/sizeof(char *); i++) {
-          // int len = strlen(keyname[i]);
-          char *key_name = buf+3;
-          int len1 = strlen(keyname[i]);
-          int len2 = strlen(key_name);
-          // printf("%s, %s, %d\n", keyname[i], key_name, len1);
-          if (len1 == len2 && !strncmp(keyname[i], key_name, len1)) {
-            event->key.keysym.sym = i;
-            // printf("i: %d, keyname: %s, keyname_i: %s\n", i, key_name, keyname[i]);
-            // printf("%s vs %s\n", keyname[i], key_name);
-          }
-      }
-    } else if(buf[0] == 'k' && buf[1] == 'u') {
-      event->type = SDL_KEYUP;
-    } else {
-      assert(0);
+    if(buf[0] == 'k' && buf[1] == 'd') event->type = SDL_KEYDOWN;
+    else if(buf[0] == 'k' && buf[1] == 'u') event->type = SDL_KEYUP;
+    else assert(0);
+    
+    for(int i = 0; i < sizeof(keyname)/sizeof(char *); i++) {
+        char *key_name = buf+3;
+        int len1 = strlen(keyname[i]);
+        int len2 = strlen(key_name);
+        if (len1 == len2 && !strncmp(keyname[i], key_name, len1)) {
+          event->key.keysym.sym = i;
+        }
     }
 
     RETN_VAL = 1;
@@ -54,23 +47,21 @@ int SDL_WaitEvent(SDL_Event *event) {
   memset(buf, 0, sizeof(buf));
   memset(event, 0, sizeof(event));
   while(!NDL_PollEvent(buf, sizeof(buf)));
-  if(buf[0] == 'k' && buf[1] == 'd') {
-    event->type = SDL_KEYDOWN;
-    for(int i = 0; i < sizeof(keyname)/sizeof(char *); i++) {
-        char *key_name = buf+3;
-        int len1 = strlen(keyname[i]);
-        int len2 = strlen(key_name);
-        // printf("%s, %s, %d\n", keyname[i], key_name, len);
-        if (len1 == len2 && !strncmp(keyname[i], key_name, len1)) {
-          event->key.keysym.sym = i;
-          // printf("%s vs %s\n", keyname[i], key_name);
-        }
-    }
-  } else if(buf[0] == 'k' && buf[1] == 'u') {
-    event->type = SDL_KEYUP;
-  } else {
-    assert(0);
-  }
+
+  if(buf[0] == 'k' && buf[1] == 'd') event->type = SDL_KEYDOWN;
+  else if(buf[0] == 'k' && buf[1] == 'u') event->type = SDL_KEYUP;
+  else assert(0);
+
+  for (int i = 0; i < sizeof(keyname)/sizeof(char *); i++) {
+     char *key_name = buf+3;
+     int len1 = strlen(keyname[i]);
+     int len2 = strlen(key_name);
+     // printf("%s, %s, %d\n", keyname[i], key_name, len);
+     if (len1 == len2 && !strncmp(keyname[i], key_name, len1)) {
+       event->key.keysym.sym = i;
+       // printf("%s vs %s\n", keyname[i], key_name);
+     }
+  } 
 
   return 1;
 }
