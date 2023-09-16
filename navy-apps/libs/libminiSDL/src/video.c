053ae5d141208f866e223f350fcb1194e3e3f512
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-16 11:51:11 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:51:11 up 12:27,  4 users,  load average: 1.52, 0.97, 1.08

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -131,6 +131,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       uint8_t r = s->format->palette->colors[idx].r;
       uint8_t g = s->format->palette->colors[idx].g;
       uint8_t b = s->format->palette->colors[idx].b;
+      uint8_t a = s->format->palette->colors[idx].a;
 
       pixels[i] = r<<24 | g<<16 | b<<8 | a;
     }
