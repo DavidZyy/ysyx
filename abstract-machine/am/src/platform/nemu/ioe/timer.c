6865f6b692429a6e4c902d08a6ba9977439a890b
DavidZyy <1929772352@qq.com>
2023-09-22 14:33:04 +0800

    save

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,31 +1,17 @@
 #include <am.h>
 #include <nemu.h>
 
-// void __am_timer_init() {
-// }
-// 
-// void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-//   uptime->us = 0;
-// }
-// 
-// void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-//   rtc->second = 0;
-//   rtc->minute = 0;
-//   rtc->hour   = 0;
-//   rtc->day    = 0;
-//   rtc->month  = 0;
-//   rtc->year   = 1900;
-// }
-
 static uint64_t boot_time = 0;
 
-#include<stdio.h>
+static uint64_t read_time() __attribute__((noinline));
 /* access can only get old data! so get error!! */
 /* the clock frequency in nemu */
 /* return the us */
 static uint64_t read_time() {
-  /* hi should put before before lo, or the rtc_io_handler will not be 
-    called, because it has "if (!is_write && offset == 4) {",  */
+  /*hi should put before before lo, or the rtc_io_handler will not be 
+    called, and the time in register will not ber updated,
+    because it has "if (!is_write && offset == 4) {",
+    the offset of hi is 4 form RTC_ADDR.  */
   uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
   uint32_t lo = *(volatile uint32_t *)(RTC_ADDR + 0);
   uint64_t time = ((uint64_t)hi << 32) | lo;
@@ -33,16 +19,12 @@ static uint64_t read_time() {
 }
 
 void __am_timer_init() {
-  // printf("\nbefore read_time __am_timer_init: %ld\n", boot_time);
   boot_time = read_time();
-  // printf("after __am_timer_init: %ld\n\n", boot_time);
 }
 
 // /* return the us */
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // printf("\nbefore read_time __am_timer_uptime: %ld\n", uptime->us);
   uptime->us = read_time() - boot_time;
-  // printf("after __am_timer_uptime: %ld\n\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
