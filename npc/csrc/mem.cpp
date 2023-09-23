bb0be7b6011b16c9ee947814bf569e143138a449
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 23:14:12 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:14:12 up  3:47,  4 users,  load average: 0.32, 0.40, 0.42

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -103,8 +103,8 @@ extern "C" void vaddr_read(sword_t raddr, sword_t *rdata) {
   pmem_read(raddr, rdata);
   // store also call read
   // if(top->io_out_is_load) {
-    // IFDEF(CONFIG_MTRACE, log_write("pc:" FMT_WORD", inst:" FMT_WORD"\n", top->io_out_pc, top->io_out_inst));
-    // IFDEF(CONFIG_MTRACE, log_write("raddr:" FMT_WORD", rdata:" FMT_WORD"\n\n", raddr, *rdata));
+    IFDEF(CONFIG_MTRACE, log_write("pc:" FMT_WORD", inst:" FMT_WORD"\n", top->io_out_pc, top->io_out_inst));
+    IFDEF(CONFIG_MTRACE, log_write("raddr:" FMT_WORD", rdata:" FMT_WORD"\n\n", raddr, *rdata));
   // }
 }
 
