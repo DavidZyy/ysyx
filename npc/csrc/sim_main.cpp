be7fdd13471e6b4b7348c1e0189bdbab4038f973
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 20:44:39 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:44:39 up  1:18,  3 users,  load average: 0.73, 0.45, 0.58

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -161,10 +161,12 @@ void init_device() {
 int status = 0;
 
 void vga_update_screen();
+extern uint8_t pmem[CONFIG_MSIZE];
 int main(int argc, char *argv[]) {
   // Assert(0, "hi:%d" , 5);
   init_monitor(argc, argv);
   init_device();
+  memset(pmem, 0, sizeof(pmem));
   Log("wave has closed to make it sim faster");
 
   sim_init();
