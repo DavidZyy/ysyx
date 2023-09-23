ea7dcd31c38076049ecc9c1e162d944dfcecb27f
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 14:00:47 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:00:47 up  4:27,  5 users,  load average: 0.55, 0.41, 0.63

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -82,7 +82,8 @@ extern "C" void pmem_write(sword_t waddr, sword_t wdata) {
     // printf("%d: %c", waddr, (char)wdata);
     printf("%c", (char)wdata);
   } else if(in_vmem(waddr)) {
-    uint8_t *vmem_addr = waddr - FB_ADDR + (uint8_t *)vmem;
+    // uint8_t *vmem_addr = waddr - FB_ADDR + (uint8_t *)vmem;
+    uint32_t *vmem_addr = waddr - FB_ADDR + (uint32_t *)vmem;
     *(uint32_t *)vmem_addr = wdata;
   } else if (waddr == VGACTL_ADDR+4) {
     vgactl_port_base[1] = wdata;
