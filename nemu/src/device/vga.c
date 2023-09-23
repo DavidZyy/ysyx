d3aa0880bfcaa1dc0e53c0e8a9291f2c2cd5a872
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 12:32:52 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:32:52 up  2:59,  5 users,  load average: 0.44, 0.54, 0.51

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -36,7 +36,7 @@ static uint32_t *vgactl_port_base = NULL;
 
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #ifndef CONFIG_TARGET_AM
-// #include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
 
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
