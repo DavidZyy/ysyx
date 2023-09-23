3988606784cd32190309c6802a99d2172595c1fa
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 17:15:15 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:15:15 up  7:41,  6 users,  load average: 0.47, 0.34, 0.35

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -186,7 +186,7 @@ int main(int argc, char *argv[]) {
     npc_exec_once();
     // if(i&7 == 0) 
     // vga_update_screen();
-    nemu_exec_once();
+    // nemu_exec_once();
     // log_write
     // log_write("pc:" FMT_WORD", inst:" FMT_WORD"\n", top->io_out_pc, top->io_out_inst);
     
