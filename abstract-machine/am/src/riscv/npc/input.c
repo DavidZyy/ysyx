094a263cc59e919aa46a9ff1de392b00f641b086
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-23 11:22:41 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:22:41 up  1:49,  5 users,  load average: 0.58, 0.85, 0.90

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -5,8 +5,9 @@
 
 /* if press a, if key down is 802b, if key up is 2b, not same as ps/2 */
 static uint32_t read_key(){
-  uint32_t key = inl(KBD_ADDR);
-  return key;
+  // uint32_t key = inl(KBD_ADDR);
+  // return key;
+  return 0;
 }
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
