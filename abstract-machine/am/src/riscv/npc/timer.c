8323d99f54146ca86d70831473a2ebf55a03dde4
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-26 15:41:22 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:41:22 up  3:41,  7 users,  load average: 0.89, 1.19, 0.97

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,30 +1,30 @@
 #include <am.h>
 #include "include/npc.h"
 
-// static uint64_t boot_time = 0;
-//  
-// #include <stdio.h>
-// static uint64_t read_time() __attribute__((noinline));
-// /* access can only get old data! so get error!! */
-// /* the clock frequency in nemu */
-// /* return the us */
-// static uint64_t read_time() {
-//   /* hi should put before before lo, or the rtc_io_handler will not be 
-//     called, because it has "if (!is_write && offset == 4) {",  */
-//   uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
-//   // printf("read hi: %x\n", hi);
-//   uint32_t lo = *(volatile uint32_t *)(RTC_ADDR + 0);
-//   // printf("read lo: %x\n", lo);
-//   uint64_t time = ((uint64_t)hi << 32) | lo;
-//   return time;
-// }
+static uint64_t boot_time = 0;
+ 
+#include <stdio.h>
+static uint64_t read_time() __attribute__((noinline));
+/* access can only get old data! so get error!! */
+/* the clock frequency in nemu */
+/* return the us */
+static uint64_t read_time() {
+  /* hi should put before before lo, or the rtc_io_handler will not be 
+    called, because it has "if (!is_write && offset == 4) {",  */
+  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
+  // printf("read hi: %x\n", hi);
+  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR + 0);
+  // printf("read lo: %x\n", lo);
+  uint64_t time = ((uint64_t)hi << 32) | lo;
+  return time;
+}
 
 void __am_timer_init() {
-  // boot_time = read_time();
+  boot_time = read_time();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // uptime->us = read_time() - boot_time;
+  uptime->us = read_time() - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
