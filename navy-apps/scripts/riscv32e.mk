8723590c55c1520b9927aa2c61d08d1f2d3726ec
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-27 16:59:32 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:59:32 up  5:56,  4 users,  load average: 0.80, 0.90, 0.95

--- navy-apps/scripts/riscv32e.mk
+++ navy-apps/scripts/riscv32e.mk
@@ -3,5 +3,5 @@ LNK_ADDR = $(if $(VME), 0x40000000, 0x83000000)
 CFLAGS  += -fno-pic -mcmodel=medany
 LDFLAGS += --no-relax -Ttext-segment $(LNK_ADDR)
 
-CFLAGS  += -march=rv32em -mabi=ilp32e  #overwrite
+CFLAGS  += -march=rv32e -mabi=ilp32e  #overwrite
 LDFLAGS += -melf32lriscv
