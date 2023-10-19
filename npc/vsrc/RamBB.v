dc44f4138117339bb7c2bbfeca3e22934ace1def
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-19 23:03:09 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:03:09 up 56 min,  4 users,  load average: 0.91, 0.83, 0.81

--- npc/vsrc/RamBB.v
+++ npc/vsrc/RamBB.v
@@ -24,6 +24,7 @@ module RamBB (
 
 // always @(*) begin
 always @(negedge clock) begin
+  $display("wmask = %b", wmask);
   if (valid) begin // 有读写请求时
     if (mem_wen) begin // 有写请求时
       vaddr_write(addr, wdata, wmask_new);
