cd6e77e0914418e67eac1377d1f5baf640930dbe
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-31 14:56:14 +0800

    > compile NEMU ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:56:14 up  4:13,  3 users,  load average: 0.66, 0.60, 0.46

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -53,7 +53,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   stack_top->mstatus = 0x1800;
   // a0 is gpr[10]
   for(int i=0; i<6; i++) {
-    stack_top->gpr[10+i] = ((uint32_t *)arg)[i];
+    stack_top->gpr[10+i] = ((uintptr_t *)arg)[i];
   }
   // printf("mepc addr:%p\n", &(stack_top->mepc));
   return stack_top;
