c6f4d51c85030ebb5acc440fb26fd7027ce89d0f
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-16 20:41:40 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:41:40 up  2:20,  2 users,  load average: 0.41, 0.70, 0.86

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -101,8 +101,9 @@ extern "C" void pmem_write(sword_t waddr, sword_t wdata, char wmask) {
     vgactl_port_base[1] = wdata;
     npc_write_device = 1;
   } else {
-    // if(!in_pmem(waddr)) out_of_bound(waddr);
-    if(!in_pmem(waddr)) return;
+    if(!in_pmem(waddr)) out_of_bound(waddr);
+    // if can not see wave to debug because of out of memory, use the following line.
+    // if(!in_pmem(waddr)) return;
 
     char addr_low = waddr & align_mask;
 
