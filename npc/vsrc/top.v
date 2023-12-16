4d1794e1fa8d8a6b48498a852283b509d1345561
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-16 18:08:06 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:08:06 up 19:56,  2 users,  load average: 1.33, 1.67, 1.43

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1603,8 +1603,6 @@ module EXU_pipeline(
   wire        _to_IFU_bits_target_T = from_ISU_bits_ctrl_sig_fu_op == 3'h3;
   wire        _to_IFU_bits_target_T_2 = from_ISU_bits_ctrl_sig_fu_op == 3'h5;
   wire        _GEN_0 = _to_IFU_bits_target_T_2 | _to_IFU_bits_target_T;
-  wire        _from_ISU_ready_output =
-    ~_GEN_0 & (_GEN | _Lsu_i_io_out_end) | ~from_ISU_valid;
   assign _to_IFU_valid_output = _to_IFU_bits_target_T_2 | _to_IFU_bits_target_T;
   Alu Alu_i (
     .io_in_src1
@@ -1672,9 +1670,9 @@ module EXU_pipeline(
   not_impl_moudle not_impl_moudle_i (
     .not_impl (from_ISU_bits_ctrl_sig_not_impl & from_ISU_valid)
   );
-  assign from_ISU_ready = _from_ISU_ready_output;
+  assign from_ISU_ready = ~_GEN_0 & (_GEN | _Lsu_i_io_out_end) | ~from_ISU_valid;
   assign to_WBU_valid =
-    _from_ISU_ready_output & from_ISU_valid
+    from_ISU_valid
     & (_GEN_0 ? to_IFU_ready & _to_IFU_valid_output : _GEN | _Lsu_i_io_out_end);
   assign to_WBU_bits_alu_result = _Alu_i_io_out_result;
   assign to_WBU_bits_pc = from_ISU_bits_pc;
