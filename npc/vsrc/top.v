272130bd3ded26d81d6720a22dade623bd2ae223
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-24 17:42:56 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:42:56 up  7:08,  2 users,  load average: 0.78, 0.73, 0.72

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -2364,16 +2364,16 @@ module CacheStage2(
   output [31:0] io_out_addr,
   output        io_out_resp_valid,
   output [31:0] io_out_resp_bits_rdata,
-                io_in_bits_addr__bore,
-  output        io_in_valid__bore
+  output        io_in_valid__bore,
+  output [31:0] io_in_bits_addr__bore
 );
 
   assign io_in_ready = io_out_resp_ready;
   assign io_out_addr = io_in_bits_addr;
   assign io_out_resp_valid = io_in_valid;
   assign io_out_resp_bits_rdata = io_in_valid ? io_dataReadBus_rdata : 32'h0;
-  assign io_in_bits_addr__bore = io_in_bits_addr;
   assign io_in_valid__bore = io_in_valid;
+  assign io_in_bits_addr__bore = io_in_bits_addr;
 endmodule
 
 module Cache(
@@ -2393,8 +2393,8 @@ module Cache(
   output [31:0] io_mem_req_bits_addr,
   output        io_mem_resp_ready,
   output [31:0] io_stage2Addr,
-                s2_io_in_bits_addr__bore,
-  output        s2_io_in_valid__bore
+  output        s2_io_in_valid__bore,
+  output [31:0] s2_io_in_bits_addr__bore
 );
 
   wire        _s2_io_in_ready;
@@ -2463,8 +2463,8 @@ module Cache(
     .io_out_addr            (io_stage2Addr),
     .io_out_resp_valid      (_s2_io_out_resp_valid),
     .io_out_resp_bits_rdata (io_in_resp_bits_rdata),
-    .io_in_bits_addr__bore  (s2_io_in_bits_addr__bore),
-    .io_in_valid__bore      (s2_io_in_valid__bore)
+    .io_in_valid__bore      (s2_io_in_valid__bore),
+    .io_in_bits_addr__bore  (s2_io_in_bits_addr__bore)
   );
   assign io_in_resp_valid = _s2_io_out_resp_valid;
 endmodule
@@ -3328,8 +3328,8 @@ module top(
   wire [31:0] _icache_io_mem_req_bits_addr;
   wire        _icache_io_mem_resp_ready;
   wire [31:0] _icache_io_stage2Addr;
-  wire [31:0] _icache_s2_io_in_bits_addr__bore;
   wire        _icache_s2_io_in_valid__bore;
+  wire [31:0] _icache_s2_io_in_bits_addr__bore;
   wire        _ram_i_axi_ar_ready;
   wire        _ram_i_axi_r_valid;
   wire [31:0] _ram_i_axi_r_bits_data;
@@ -3448,6 +3448,16 @@ module top(
   reg  [2:0]  EXU_i_from_ISU_bits_r_ctrl_sig_csr_op;
   reg  [3:0]  EXU_i_from_ISU_bits_r_ctrl_sig_mdu_op;
   reg  [31:0] EXU_i_from_ISU_bits_r_inst;
+  reg         valid_3;
+  reg  [31:0] WBU_i_from_EXU_bits_r_alu_result;
+  reg  [31:0] WBU_i_from_EXU_bits_r_mdu_result;
+  reg  [31:0] WBU_i_from_EXU_bits_r_lsu_rdata;
+  reg  [31:0] WBU_i_from_EXU_bits_r_csr_rdata;
+  reg  [31:0] WBU_i_from_EXU_bits_r_pc;
+  reg         WBU_i_from_EXU_bits_r_reg_wen;
+  reg  [4:0]  WBU_i_from_EXU_bits_r_rd;
+  reg  [2:0]  WBU_i_from_EXU_bits_r_fu_op;
+  reg  [31:0] WBU_i_from_EXU_bits_r_inst;
   wire        _GEN = _ISU_i_from_IDU_ready & _IDU_i_to_ISU_valid;
   wire        _GEN_0 =
     _EXU_i_to_IFU_bits_redirect & _IDU_i_from_IFU_ready & _IFU_i_to_IDU_valid;
@@ -3462,11 +3472,13 @@ module top(
       valid <= 1'h0;
       valid_1 <= 1'h0;
       valid_2 <= 1'h0;
+      valid_3 <= 1'h0;
     end
     else begin
       valid <= ~_GEN_0 & (_IDU_i_from_IFU_bits_T_1 | ~_GEN & valid);
       valid_1 <= ~_GEN_2 & (_ISU_i_from_IDU_bits_T_1 | ~_GEN_1 & valid_1);
       valid_2 <= ~_GEN_3 & (_EXU_i_from_ISU_bits_T_1 | ~_EXU_i_to_WBU_valid & valid_2);
+      valid_3 <= _EXU_i_to_WBU_valid;
     end
     if (_IDU_i_from_IFU_bits_T_1) begin
       if (_GEN_0) begin
@@ -3561,6 +3573,17 @@ module top(
         ~_GEN_3 & _ISU_i_to_EXU_bits_ctrl_sig_is_ebreak;
       EXU_i_from_ISU_bits_r_ctrl_sig_not_impl <=
         ~_GEN_3 & _ISU_i_to_EXU_bits_ctrl_sig_not_impl;
+    end
+    if (_EXU_i_to_WBU_valid) begin
+      WBU_i_from_EXU_bits_r_alu_result <= _EXU_i_to_WBU_bits_alu_result;
+      WBU_i_from_EXU_bits_r_mdu_result <= _EXU_i_to_WBU_bits_mdu_result;
+      WBU_i_from_EXU_bits_r_lsu_rdata <= _EXU_i_to_WBU_bits_lsu_rdata;
+      WBU_i_from_EXU_bits_r_csr_rdata <= _EXU_i_to_WBU_bits_csr_rdata;
+      WBU_i_from_EXU_bits_r_pc <= _EXU_i_to_WBU_bits_pc;
+      WBU_i_from_EXU_bits_r_reg_wen <= _EXU_i_to_WBU_bits_reg_wen;
+      WBU_i_from_EXU_bits_r_rd <= _EXU_i_to_WBU_bits_rd;
+      WBU_i_from_EXU_bits_r_fu_op <= _EXU_i_to_WBU_bits_fu_op;
+      WBU_i_from_EXU_bits_r_inst <= _EXU_i_to_WBU_bits_inst;
     end
   end // always @(posedge)
   IDU IDU_i (
@@ -3695,15 +3718,15 @@ module top(
     .to_ISU_isBR                      (_EXU_i_to_ISU_isBR)
   );
   WBU WBU_i (
-    .from_EXU_valid           (_EXU_i_to_WBU_valid),
-    .from_EXU_bits_alu_result (_EXU_i_to_WBU_bits_alu_result),
-    .from_EXU_bits_mdu_result (_EXU_i_to_WBU_bits_mdu_result),
-    .from_EXU_bits_lsu_rdata  (_EXU_i_to_WBU_bits_lsu_rdata),
-    .from_EXU_bits_csr_rdata  (_EXU_i_to_WBU_bits_csr_rdata),
-    .from_EXU_bits_pc         (_EXU_i_to_WBU_bits_pc),
-    .from_EXU_bits_reg_wen    (_EXU_i_to_WBU_bits_reg_wen),
-    .from_EXU_bits_rd         (_EXU_i_to_WBU_bits_rd),
-    .from_EXU_bits_fu_op      (_EXU_i_to_WBU_bits_fu_op),
+    .from_EXU_valid           (valid_3),
+    .from_EXU_bits_alu_result (WBU_i_from_EXU_bits_r_alu_result),
+    .from_EXU_bits_mdu_result (WBU_i_from_EXU_bits_r_mdu_result),
+    .from_EXU_bits_lsu_rdata  (WBU_i_from_EXU_bits_r_lsu_rdata),
+    .from_EXU_bits_csr_rdata  (WBU_i_from_EXU_bits_r_csr_rdata),
+    .from_EXU_bits_pc         (WBU_i_from_EXU_bits_r_pc),
+    .from_EXU_bits_reg_wen    (WBU_i_from_EXU_bits_r_reg_wen),
+    .from_EXU_bits_rd         (WBU_i_from_EXU_bits_r_rd),
+    .from_EXU_bits_fu_op      (WBU_i_from_EXU_bits_r_fu_op),
     .to_ISU_bits_reg_wen      (_WBU_i_to_ISU_bits_reg_wen),
     .to_ISU_bits_wdata        (_WBU_i_to_ISU_bits_wdata),
     .to_ISU_bits_rd           (_WBU_i_to_ISU_bits_rd)
@@ -3764,8 +3787,8 @@ module top(
     .io_mem_req_bits_addr     (_icache_io_mem_req_bits_addr),
     .io_mem_resp_ready        (_icache_io_mem_resp_ready),
     .io_stage2Addr            (_icache_io_stage2Addr),
-    .s2_io_in_bits_addr__bore (_icache_s2_io_in_bits_addr__bore),
-    .s2_io_in_valid__bore     (_icache_s2_io_in_valid__bore)
+    .s2_io_in_valid__bore     (_icache_s2_io_in_valid__bore),
+    .s2_io_in_bits_addr__bore (_icache_s2_io_in_bits_addr__bore)
   );
   SimpleBus2AXI4Converter bridge (
     .io_in_req_valid       (_icache_io_mem_req_valid),
@@ -3884,9 +3907,9 @@ module top(
   assign io_out_idu_pc = _IDU_i_to_ISU_bits_pc;
   assign io_out_exu_inst = _EXU_i_to_WBU_bits_inst;
   assign io_out_exu_pc = _EXU_i_to_WBU_bits_pc;
-  assign io_out_wbu_inst = _EXU_i_to_WBU_bits_inst;
-  assign io_out_wbu_pc = _EXU_i_to_WBU_bits_pc;
-  assign io_out_wb = _EXU_i_to_WBU_valid;
+  assign io_out_wbu_inst = WBU_i_from_EXU_bits_r_inst;
+  assign io_out_wbu_pc = WBU_i_from_EXU_bits_r_pc;
+  assign io_out_wb = valid_3;
 endmodule
 
 
