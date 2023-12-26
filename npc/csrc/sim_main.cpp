ba422d1983787acbac8292727abd4844b90ef4f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-26 19:51:54 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:51:54 up  7:08,  2 users,  load average: 2.25, 1.71, 1.47

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -208,7 +208,7 @@ void get_text_addr_range(const char *elf_file) {
 static void statistic() {
   Log("host cpu cycle spent = %u" , g_cycle);
   Log("total guest instructions = %u",  g_nr_guest_inst);
-  if (g_cycle > 0) Log("ipc = %d ", (double)(g_nr_guest_inst / g_cycle));
+  if (g_cycle > 0) Log("ipc = %d ", (double)g_nr_guest_inst / (double)g_cycle);
   else Log("Finish running in less than 1 cycle and can not calculate the ipc");
 }
 
