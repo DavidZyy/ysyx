e23b5a0a9cf3c57b8da4ac05ddbfd47eab76c379
tracer-ysyx2204 <tracer@ysyx.org>
2024-01-07 19:33:46 +0800

    > sim RTL ysyx_22050926 朱洋洋 Linux vm 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:33:46 up  9:08,  2 users,  load average: 1.42, 1.08, 0.88

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1572,9 +1572,10 @@ module EXU_pipeline(
   wire        _to_WBU_bits_is_mmio_T = from_ISU_bits_ctrl_sig_fu_op == 3'h4;
   wire        _GEN = from_ISU_bits_ctrl_sig_fu_op != 3'h4;
   wire        taken = _Bru_i_io_out_ctrl_br | _Csr_i_io_out_csr_br;
-  wire [31:0] _to_WBU_bits_redirect_target_T_3 = from_ISU_bits_pc + 32'h20;
+  wire [31:0] _predictWrong_T_5 = from_ISU_bits_pc + 32'h4;
   wire        _to_ISU_hazard_isBR_T = from_ISU_bits_ctrl_sig_fu_op == 3'h3;
   wire        _to_ISU_hazard_isBR_T_1 = from_ISU_bits_ctrl_sig_fu_op == 3'h5;
+  wire [31:0] _to_WBU_bits_redirect_target_T_3 = from_ISU_bits_pc + 32'h20;
   Alu Alu_i (
     .io_in_src1
       (from_ISU_bits_ctrl_sig_src1_op == 2'h1
@@ -1640,8 +1641,7 @@ module EXU_pipeline(
   assign to_WBU_bits_rd = from_ISU_bits_rd;
   assign to_WBU_bits_fu_op = from_ISU_bits_ctrl_sig_fu_op;
   assign to_WBU_bits_redirect_valid =
-    (taken & npc == _to_WBU_bits_redirect_target_T_3 | ~taken
-     & npc != _to_WBU_bits_redirect_target_T_3)
+    (taken & npc == _predictWrong_T_5 | ~taken & npc != _predictWrong_T_5)
     & (_to_ISU_hazard_isBR_T | _to_ISU_hazard_isBR_T_1) & from_ISU_valid;
   assign to_WBU_bits_redirect_target =
     from_ISU_bits_ctrl_sig_fu_op == 3'h5
@@ -2857,8 +2857,8 @@ module CacheStage2(
   output        io_dataWriteBus_req_valid,
   output [8:0]  io_dataWriteBus_req_bits_waddr,
   output [31:0] io_dataWriteBus_req_bits_wdata,
-                io_in_bits_addr__bore,
-  output        io_in_valid__bore
+  output        io_in_valid__bore,
+  output [31:0] io_in_bits_addr__bore
 );
 
   wire [31:0] _io_dataWriteBus_req_bits_wdata_T_12 =
@@ -2875,8 +2875,8 @@ module CacheStage2(
   assign io_dataWriteBus_req_bits_wdata =
     io_in_bits_wdata & _io_dataWriteBus_req_bits_wdata_T_12 | io_dataReadBus_rdata
     & ~_io_dataWriteBus_req_bits_wdata_T_12;
-  assign io_in_bits_addr__bore = io_in_bits_addr;
   assign io_in_valid__bore = io_in_valid;
+  assign io_in_bits_addr__bore = io_in_bits_addr;
 endmodule
 
 module Arbiter2_SRAMBundleWriteReq(
@@ -2914,8 +2914,8 @@ module Cache(
                 io_mem_req_bits_wdata,
   output [3:0]  io_mem_req_bits_cmd,
   output [31:0] io_stage2Addr,
-                s2_io_in_bits_addr__bore,
-  output        s2_io_in_valid__bore
+  output        s2_io_in_valid__bore,
+  output [31:0] s2_io_in_bits_addr__bore
 );
 
   wire        _dataWriteArb_io_out_valid;
@@ -3024,8 +3024,8 @@ module Cache(
     .io_dataWriteBus_req_valid      (_s2_io_dataWriteBus_req_valid),
     .io_dataWriteBus_req_bits_waddr (_s2_io_dataWriteBus_req_bits_waddr),
     .io_dataWriteBus_req_bits_wdata (_s2_io_dataWriteBus_req_bits_wdata),
-    .io_in_bits_addr__bore          (s2_io_in_bits_addr__bore),
-    .io_in_valid__bore              (s2_io_in_valid__bore)
+    .io_in_valid__bore              (s2_io_in_valid__bore),
+    .io_in_bits_addr__bore          (s2_io_in_bits_addr__bore)
   );
   Arbiter2_SRAMBundleWriteReq dataWriteArb (
     .io_in_0_valid      (_s1_io_dataWriteBus_req_valid),
@@ -3435,8 +3435,8 @@ module top(
   wire [31:0] _icache_io_mem_req_bits_wdata;
   wire [3:0]  _icache_io_mem_req_bits_cmd;
   wire [31:0] _icache_io_stage2Addr;
-  wire [31:0] _icache_s2_io_in_bits_addr__bore;
   wire        _icache_s2_io_in_valid__bore;
+  wire [31:0] _icache_s2_io_in_bits_addr__bore;
   wire        _ram_i_axi_ar_ready;
   wire        _ram_i_axi_r_valid;
   wire [31:0] _ram_i_axi_r_bits_data;
@@ -3953,8 +3953,8 @@ module top(
     .io_mem_req_bits_wdata    (_icache_io_mem_req_bits_wdata),
     .io_mem_req_bits_cmd      (_icache_io_mem_req_bits_cmd),
     .io_stage2Addr            (_icache_io_stage2Addr),
-    .s2_io_in_bits_addr__bore (_icache_s2_io_in_bits_addr__bore),
-    .s2_io_in_valid__bore     (_icache_s2_io_in_valid__bore)
+    .s2_io_in_valid__bore     (_icache_s2_io_in_valid__bore),
+    .s2_io_in_bits_addr__bore (_icache_s2_io_in_bits_addr__bore)
   );
   SimpleBus2AXI4Converter bridge (
     .io_in_req_valid       (_icache_io_mem_req_valid),
